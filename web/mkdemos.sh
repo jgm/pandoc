8c98a36b33ca2c72c46b099f0fe6945256da22d7
fiddlosopher <fiddlosopher@788f1e2b-df1e-0410-8736-df70ead52e1b>
2007-01-09 02:06:57 +0000

    Improvements in website: + Descriptions on examples. + New "features" page highlighting Pandoc's features. + Small other improvements.
    git-svn-id: https://pandoc.googlecode.com/svn/trunk@466 788f1e2b-df1e-0410-8736-df70ead52e1b

--- web/mkdemos.sh
+++ web/mkdemos.sh
@@ -8,17 +8,29 @@ PROGPATH=$2
 
 NEWLINE='
 '
-EXAMPLES='pandoc README -o example0.html
+EXAMPLES='HTML fragment:
+pandoc README -o example0.html
+Standalone HTML file:
 pandoc -s -S README -o example0.html
+HTML with smart quotes, CSS, and custom header and footer:
 pandoc -s -S -c main.css -B header.html -A footer.html README -o example0.html
+LaTeX:
 pandoc -s README -o example0.tex
+From LaTeX to markdown:
 pandoc -s README.tex -o example0.txt
+reStructuredText:
 pandoc -s -w rst README -o example0.txt
+Rich text format (RTF):
 pandoc -s README -o example0.rtf 
+S5 HTML slide show (all in one file):
 pandoc -s -m -i -w s5 S5DEMO -o example0.html
+Docbook XML:
 pandoc -s -w docbook README -o example0.db
+Converting a web page to markdown:
 html2markdown http://www.gnu.org/software/make/ -o example0.txt
+From markdown to PDF:
 markdown2pdf README -o example0.pdf
+Using a custom LaTeX header to change the font style of the PDF:
 markdown2pdf -C myheader.tex README -o example0.pdf'
 
 oldifs=$IFS
@@ -35,14 +47,20 @@ click on the name of the output file:
 
 '
 num=0
-for command in "$@"; do
+while [ $# -gt 0 ]; do
+    description="$1"
+    command="$2"
     num=$(($num + 1))
     command=$(echo $command | sed -e "s/0/$num/")
     firstpart=$(echo $command | sed -e 's/\(.*\) [^ ]* -o.*/\1/')
     input=$(echo $command | sed -e 's/.* \([^ ]*\) -o.*/\1/') 
     output=$(echo $command | sed -e 's/.*-o \(.*\)/\1/')
-    echo "1. <code>$firstpart <a href=\""$input"\" title=\""View input file"\">$input</a> -o <a href=\""$output"\" title=\""View pandoc output"\">$output</a></code>"
+    echo "1. $description"
+    echo
+    echo "    <code>$firstpart <a href=\""$input"\" title=\""View input file"\">$input</a> -o <a href=\""$output"\" title=\""View pandoc output"\">$output</a></code>"
     echo $command >&2
     result=$(PATH=$PROGPATH:$PATH $command) # run the command and create output file
+    shift
+    shift
 done
 
