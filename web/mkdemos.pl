e7dabc9dd0ed0bb2578677e893d7479ddbd2d1c6
fiddlosopher <fiddlosopher@788f1e2b-df1e-0410-8736-df70ead52e1b>
2007-01-10 02:33:33 +0000

    More Changes to website target.  Moved to a templating system for the examples page.
    git-svn-id: https://pandoc.googlecode.com/svn/trunk@487 788f1e2b-df1e-0410-8736-df70ead52e1b

---
+++ web/mkdemos.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+# first argument is input filename - a demo template.
+# second argument is output filename.
+
+my $infile=$ARGV[0];
+my $outfile=$ARGV[1];
+
+open( IN, "< $infile" );
+open( OUT, "> $outfile" );
+
+while (<IN>) {
+
+    my $line = $_;
+    my $firstchar = substr ($line,0,1);
+    if ( $firstchar eq '@' ) {
+        my $command = substr ($line,4);
+        print STDERR "$command";
+        system "$command";
+        $line = $command;
+        $line =~ s/-/\-/;
+        $line =~ s/ ([A-Za-z0-9_:\/]+(\.|\/)[a-zA-Z0-9.\/]*|README|S5DEMO)/ <a href="$1">$1<\/a>/g;
+        $line =~ s/-/\\-/g;
+        $line =~ s/^(.*)$/    <code>$1<\/code>/g;        
+    }
+    print OUT $line;
+
+}
