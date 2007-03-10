0ce965f34c0caf70e9a6a7b88508beec1e617fbb
fiddlosopher <fiddlosopher@788f1e2b-df1e-0410-8736-df70ead52e1b>
2007-03-10 17:45:00 +0000

    Modified prettyPandoc to handle DefinitionList elements.
    git-svn-id: https://pandoc.googlecode.com/svn/trunk@565 788f1e2b-df1e-0410-8736-df70ead52e1b

--- src/Text/Pandoc/Shared.hs
+++ src/Text/Pandoc/Shared.hs
@@ -190,6 +190,10 @@ prettyBlock (OrderedList blockLists) =
 prettyBlock (BulletList blockLists) = "BulletList\n" ++ 
    indentBy 2 0 ("[ " ++ (joinWithSep ", " 
    (map (\blocks -> prettyBlockList 2 blocks) blockLists))) ++ " ]" 
+prettyBlock (DefinitionList blockLists) = "DefinitionList\n" ++ 
+   indentBy 2 0 ("[ " ++ (joinWithSep ", " 
+   (map (\(term, blocks) -> "(" ++ show term ++ ",\n" ++ 
+   indentBy 1 2 (prettyBlockList 2 blocks) ++ ")") blockLists))) ++ " ]" 
 prettyBlock block = show block
 
 -- | Prettyprint Pandoc document.
