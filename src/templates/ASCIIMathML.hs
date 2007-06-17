4bf464ce4c15f9e5dc75a8c775c3b0202b552c9b
fiddlosopher <fiddlosopher@788f1e2b-df1e-0410-8736-df70ead52e1b>
2007-06-17 16:12:23 +0000

    Minor documentation changes to ASCIIMathML.hs template.
    git-svn-id: https://pandoc.googlecode.com/svn/trunk@602 788f1e2b-df1e-0410-8736-df70ead52e1b

--- src/templates/ASCIIMathML.hs
+++ src/templates/ASCIIMathML.hs
@@ -1,7 +1,7 @@
--- | Definitions for use of Pandoc.ASCIIMathML in HTML.  
+-- | Definitions for use of ASCIIMathML in HTML.  
 -- (See <http://www1.chapman.edu/~jipsen/mathml/asciimath.html>.)
 module Text.Pandoc.ASCIIMathML ( asciiMathMLScript ) where
 
--- | String containing Pandoc.ASCIIMathML javascript.
+-- | String containing ASCIIMathML javascript.
 asciiMathMLScript :: String
 asciiMathMLScript = "<script type=\"text/javascript\">\n@ASCIIMathML.js@</script>\n"
