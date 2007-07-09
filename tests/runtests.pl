8bbd11bf210b33dfe9846b83be8d6bd108cef2b9
fiddlosopher <fiddlosopher@788f1e2b-df1e-0410-8736-df70ead52e1b>
2007-07-09 02:19:15 +0000

    + Cleaned up runtests.pl.  Incorporated table tests in loop with   main writer tests. + Renamed tables.tex to tables.latex, tables.db to tables.docbook,   for consistency with the writer tests.
    git-svn-id: https://pandoc.googlecode.com/svn/trunk@661 788f1e2b-df1e-0410-8736-df70ead52e1b

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -38,24 +38,28 @@ sub test_results
 foreach my $format (@writeformats)
 {
     $options = "";
-    if ($format =~ /smart\./)
-    {
-        $options = "-S ";
-    }
 
     my $extension = $format;
-    $extension =~ s/smart\.//g;
-
     print "Testing $format writer...";
+
     `$script -r native -w $extension $options -s $source > tmp.$extension`;
 
     test_results("$format writer", "tmp.$extension", "writer.$format");
+
+    print "        $format tables...";
+
+    `$script -r native -w $extension tables.native > tmp.$extension`;
+
+    test_results("$format writer", "tmp.$extension", "tables.$format");
 }
 
 print "Testing docbook writer...";
 # remove HTML block tests, as this produces invalid docbook...
 `sed -e '/^, Header 1 \\[Str "HTML",Space,Str "Blocks"\\]/,/^, HorizontalRule/d' testsuite.native | $script -r native -w docbook -s > tmp.docbook`;
 test_results("docbook writer", "tmp.docbook", "writer.docbook");
+`$script -r native -w docbook tables.native > tmp.docbook`;
+print "        docbook tables...";
+test_results("docbook tables", "tmp.docbook", "tables.docbook");
 
 print "Testing s5 writer (basic)...";
 `$script -r native -w s5 -s s5.native > tmp.html`;
@@ -73,44 +77,11 @@ print "Testing -H -B -A -c options...";
 `$script -r native -s -w html -H insert -B insert -A insert -c main.css s5.native > tmp.html`;
 test_results("-B, -A, -H, -c options", "tmp.html", "s5.inserts.html");
 
-print "Testing tables:\n";
-print "   html writer...";
-`$script -r native -w html tables.native > tmp.html`;
-test_results("html table writer", "tmp.html", "tables.html");
-
-print "   latex writer...";
-`$script -r native -w latex tables.native > tmp.tex`;
-test_results("latex table writer", "tmp.tex", "tables.tex");
-
-print "   docbook writer...";
-`$script -r native -w docbook tables.native > tmp.db`;
-test_results("docbook table writer", "tmp.db", "tables.db");
-
-print "   rtf writer...";
-`$script -r native -w rtf tables.native > tmp.rtf`;
-test_results("rtf table writer", "tmp.rtf", "tables.rtf");
-
-print "   markdown writer...";
-`$script -r native -w markdown tables.native > tmp.markdown`;
-test_results("markdown table writer", "tmp.markdown", "tables.markdown");
-
-print "   rst writer...";
-`$script -r native -w rst tables.native > tmp.rst`;
-test_results("rst table writer", "tmp.rst", "tables.rst");
-
-print "   man writer...";
-`$script -r native -w man tables.native > tmp.man`;
-test_results("man table writer", "tmp.man", "tables.man");
-
 print "\nReader tests:\n";
 
 print "Testing markdown reader...";
 `$script -r markdown -w native -s -S testsuite.txt > tmp.native`;
 test_results("markdown reader", "tmp.native", "testsuite.native");
-
-print "   tables...";
-`$script -r markdown -w native tables.txt > tmp.native`;
-test_results("markdown table reader", "tmp.native", "tables.native");
 
 print "Testing rst reader...";
 `$script -r rst -w native -s rst-reader.rst > tmp.native`;
