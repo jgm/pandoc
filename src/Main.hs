8919f20abe37776450517a4d18c6485d7478ae0b
fiddlosopher <fiddlosopher@788f1e2b-df1e-0410-8736-df70ead52e1b>
2007-08-30 19:16:12 +0000

    Version bump to 0.43.
    git-svn-id: https://pandoc.googlecode.com/svn/trunk@962 788f1e2b-df1e-0410-8736-df70ead52e1b

--- src/Main.hs
+++ src/Main.hs
@@ -43,7 +43,7 @@ import Data.Char ( toLower )
 import Control.Monad ( (>>=) )
 
 version :: String
-version = "0.42"
+version = "0.43"
 
 copyrightMessage :: String
 copyrightMessage = "\nCopyright (C) 2006-7 John MacFarlane\n\
