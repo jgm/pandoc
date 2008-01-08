8fca649d055a0a202001bf6ff2df2b3d4e459e70
fiddlosopher <fiddlosopher@788f1e2b-df1e-0410-8736-df70ead52e1b>
2008-01-08 17:26:16 +0000

    Changed copyright dates where appropriate to include 2008.
    git-svn-id: https://pandoc.googlecode.com/svn/trunk@1181 788f1e2b-df1e-0410-8736-df70ead52e1b

--- Text/Pandoc/Writers/ConTeXt.hs
+++ Text/Pandoc/Writers/ConTeXt.hs
@@ -1,5 +1,5 @@
 {-
-Copyright (C) 2007 John MacFarlane <jgm@berkeley.edu>
+Copyright (C) 2007-8 John MacFarlane <jgm@berkeley.edu>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 {- |
    Module      : Text.Pandoc.Writers.ConTeXt
-   Copyright   : Copyright (C) 2007 John MacFarlane
+   Copyright   : Copyright (C) 2007-8 John MacFarlane
    License     : GNU GPL, version 2 or above 
 
    Maintainer  : John MacFarlane <jgm@berkeley.edu>
