7cf373dd8877189d8b8ccfc4c67738862e1f9a57
fiddlosopher <fiddlosopher@788f1e2b-df1e-0410-8736-df70ead52e1b>
2010-01-17 00:37:44 +0000

    New batch file to make-windows-installer.
    + Removed old Makefile.windows
    + Added make-windows-installer.bat
    + Modified default installer name in pandoc-setup.iss
    
    git-svn-id: https://pandoc.googlecode.com/svn/trunk@1815 788f1e2b-df1e-0410-8736-df70ead52e1b

---
+++ windows/make-windows-installer.bat
@@ -0,0 +1,15 @@
+@echo off
+cd ..
+cabal clean
+rem note: we use -f-library in building pandoc, because
+rem if the library is built, the data file paths will not be relocatable!
+cabal configure -f-library -fwrappers -fhighlighting --datasubdir=
+cabal build
+strip dist\build\pandoc\pandoc.exe
+strip dist\build\hsmarkdown\hsmarkdown.exe
+strip dist\build\markdown2pdf\markdown2pdf.exe
+dist\build\pandoc\pandoc.exe -s --template templates\html.template -S README -o README.html
+copy COPYING COPYING.txt
+copy COPYRIGHT COPYRIGHT.txt
+cd windows
+ISCC pandoc-setup.iss
