68d38db19468c6a9bb7a4676dd072e7f81f619f2
John MacFarlane <jgm@berkeley.edu>
2010-11-28 10:02:40 -0800

    Revamped tests, using markdown output instead of HTML.
    This is easier to inspect.

--- tests/RunTests.hs
+++ tests/RunTests.hs
@@ -107,7 +107,7 @@ main = do
              "latex-reader.latex" "latex-reader.native"
   r11 <- runTest "native reader" ["-r", "native", "-w", "native", "-s"]
              "testsuite.native" "testsuite.native"
-  r14s <- mapM (\style -> runTest ("markdown reader (citations) (" ++ style ++ ")") ["-r", "markdown", "-w", "html", "--bibliography", "biblio.bib", "--csl", style ++ ".csl"] "markdown-citations.txt" ("markdown-citations." ++ style ++ ".html")) ["chicago-author-date","ieee","mhra"]
+  r14s <- mapM (\style -> runTest ("markdown reader (citations) (" ++ style ++ ")") ["-r", "markdown", "-w", "markdown", "--bibliography", "biblio.bib", "--csl", style ++ ".csl", "--no-wrap"] "markdown-citations.txt" ("markdown-citations." ++ style ++ ".txt")) ["chicago-author-date","ieee","mhra"]
   r12s <- if runLhsTests
              then mapM runLhsWriterTest lhsWriterFormats
              else putStrLn "Skipping lhs writer tests because they presuppose highlighting support" >> return []
