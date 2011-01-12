ff74c51b532f05303343b4c9de3a8c392298c014
Nathan Gass <gass@search.ch>
2011-01-12 14:44:32 +0100

    Remove some accidentally commited functions. Fixed a type and alignment.

--- tests/Helpers.hs
+++ tests/Helpers.hs
@@ -12,11 +12,11 @@ data Expect = Inline  Inline
             | Blocks  [Block]
 
 assertPandoc :: Expect -> Pandoc -> Assertion
-assertPandoc (Inline e)  (Pandoc _ [Para [g]]) = e @=? g
+assertPandoc (Inline  e) (Pandoc _ [Para [g]]) = e @=? g
 assertPandoc (Inlines e) (Pandoc _ [Para g]  ) = e @=? g
-assertPandoc (Block e)   (Pandoc _ [g]       ) = e @=? g
-assertPandoc (Blocks e)  (Pandoc _ g         ) = e @=? g
-assertPandoc _ _ = assertFailure "Wrong structur of Pandoc document."
+assertPandoc (Block   e) (Pandoc _ [g]       ) = e @=? g
+assertPandoc (Blocks  e) (Pandoc _ g         ) = e @=? g
+assertPandoc _ _ = assertFailure "Wrong structure of Pandoc document."
 
 latexTest :: String-> String -> Expect -> Test
 latexTest = latexTestWithState defaultParserState
@@ -24,14 +24,3 @@ latexTest = latexTestWithState defaultParserState
 latexTestWithState :: ParserState -> String -> String -> Expect -> Test
 latexTestWithState state name string exp = testCase name $ exp `assertPandoc` readLaTeX state string 
 
-blocks :: [Block] -> Pandoc
-blocks bs = Pandoc (Meta { docTitle = [], docAuthors = [], docDate = [] }) bs
-
-block :: Block -> Pandoc
-block b = blocks [b]
-
-inlines :: [Inline] -> Pandoc
-inlines is = block $ Para is
-
-inline :: Inline -> Pandoc
-inline i = inlines [i]
