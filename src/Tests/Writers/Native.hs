1a19f96a5b9b65115323033f567a225dd479bf60
John MacFarlane <jgm@berkeley.edu>
2011-02-04 18:33:32 -0800

    Native writer test:  in block list test, limit to list < 20 blocks.

--- src/Tests/Writers/Native.hs
+++ src/Tests/Writers/Native.hs
@@ -11,8 +11,9 @@ p_write_rt d =
   read (writeNative defaultWriterOptions{ writerStandalone = True } d) == d
 
 p_write_blocks_rt :: [Block] -> Bool
-p_write_blocks_rt bs =
-  read (writeNative defaultWriterOptions (Pandoc (Meta [] [] []) bs)) == bs
+p_write_blocks_rt bs = length bs > 20 ||
+  read (writeNative defaultWriterOptions (Pandoc (Meta [] [] []) bs)) ==
+  bs
 
 tests :: [Test]
 tests = [ property "p_write_rt" p_write_rt
