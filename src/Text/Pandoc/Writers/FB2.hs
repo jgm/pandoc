4a1f0db381a9f5b026db98169192be56deac64e7
Sergey Astanin <s.astanin@gmail.com>
2011-02-20 00:49:57 +0100

    Support footnotes in FB2 writer.

--- src/Text/Pandoc/Writers/FB2.hs
+++ src/Text/Pandoc/Writers/FB2.hs
@@ -45,15 +45,14 @@ import Text.Pandoc.Generic (bottomUp)
 
 -- | Data to be written at the end of the document: (foot)notes, URLs, references, images.
 data FBNotes = FBNotes
-    { footnotes :: [ Content ]
-    , fnNumber :: Int           -- ^ number of processed footnotes so far
+    { footnotes :: [ (Int, [Content]) ]
     } deriving (Show)
 
 -- | FictionBook building monad.
 type FBM = State FBNotes
 
 newFB :: FBNotes
-newFB = FBNotes { footnotes = [], fnNumber = 0 }
+newFB = FBNotes { footnotes = [] }
 
 -- | Produce an FB2 document from a 'Pandoc' document.
 writeFB2 :: WriterOptions    -- ^ conversion options
@@ -64,15 +63,16 @@ writeFB2 _ (Pandoc meta blocks) = flip evalState newFB $ do
      fp <- frontpage meta
      secs <- renderSections 1 blocks
      let body = el "body" $ fp ++ secs
-     let fb2_xml = el "FictionBook" (fb2_attrs, [desc, body])
+     notes <- renderFootnotes
+     let fb2_xml = el "FictionBook" (fb2_attrs, [desc, body] ++ notes)
      return $ xml_head ++ (showContent fb2_xml)
   where
   xml_head = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
   fb2_attrs =
       let xmlns = "http://www.gribuser.ru/xml/fictionbook/2.0"
           xlink = "http://www.w3.org/1999/xlink"
-      in  [ Attr (QName "xmlns" Nothing Nothing) xmlns
-          , Attr (QName "l" Nothing (Just "xmlns")) xlink ]
+      in  [ uattr "xmlns" xmlns
+          , attr ("xmlns", "l") xlink ]
   --
   frontpage :: Meta -> FBM [Content]
   frontpage meta' = do
@@ -139,7 +139,7 @@ renderSection level (ttl, bs) = do
               else renderSections (level + 1) bs
     return $ el "section" (title ++ content)
 
--- Divide the stream of block elements into sections: [(title, blocks)].
+-- | Divide the stream of block elements into sections: [(title, blocks)].
 splitSections :: Int -> [Block] -> [([Inline], [Block])]
 splitSections level blocks = reverse $ revSplit (reverse blocks)
   where
@@ -153,6 +153,23 @@ splitSections level blocks = reverse $ revSplit (reverse blocks)
            else [([], reverse lastsec)]
   sameLevel (Header n _) = n == level
   sameLevel _ = False
+
+-- | Make another FictionBook body with footnotes.
+renderFootnotes :: FBM [Content]
+renderFootnotes = do
+  fns <- footnotes `liftM` get
+  if null fns
+    then return []  -- no footnotes
+    else return . list $
+         el "body" ([uattr "name" "notes"], map renderFN (reverse fns))
+  where
+    renderFN (n, cs) =
+        let fn_texts = (el "title" (el "p" (show n))) : cs
+        in  el "section" ([uattr "id" (footnoteID n)], fn_texts)
+
+
+footnoteID :: Int -> String
+footnoteID i = "note" ++ (show i)
 
 -- | Convert a block-level Pandoc's element to FictionBook XML representation.
 blockToXml :: Block -> FBM [Content]
@@ -233,7 +250,17 @@ toXml (Math _ s) = return [el "code" s] -- FIXME: generate formula images
 toXml (RawInline _ _) = return []  -- raw TeX and raw HTML are suppressed
 toXml (Link text (_,_)) = cMapM toXml text -- FIXME: url in footnotes
 toXml (Image alt (_,_)) = cMapM toXml alt  -- FIXME: embed images
-toXml (Note _) = return [txt ""] -- FIXME: implement footnotes
+toXml (Note bs) = do
+  state <- get
+  let fns = footnotes state
+  let n = 1 + length fns
+  let fn_id = footnoteID n
+  fn_desc <- cMapM blockToXml bs
+  put state { footnotes = (n, fn_desc) : fns }
+  let fn_ref = el "sup" . txt $ "[" ++ show n ++ "]"
+  return . list $ el "a" ( [ attr ("l","href") ('#':fn_id)
+                           , uattr "type" "note" ]
+                         , fn_ref )
 
 -- | Wrap all inlines with an XML tag (given its unqualified name).
 wrap :: String -> [Inline] -> FBM Content
@@ -277,6 +304,14 @@ el name cs = Elem $ unode name cs
 -- | Create a plain-text XML content.
 txt :: String -> Content
 txt s = Text $ CData CDataText s Nothing
+
+-- | Create an XML attribute with an unqualified name.
+uattr :: String -> String -> Text.XML.Light.Attr
+uattr name val = Attr (QName name Nothing Nothing) val
+
+-- | Create an XML attribute with a qualified name from given namespace.
+attr :: (String, String) -> String -> Text.XML.Light.Attr
+attr (ns, name) val = Attr (QName name Nothing (Just ns)) val
 
 -- | Abbreviation for 'concatMap'.
 cMap :: (a -> [b]) -> [a] -> [b]
