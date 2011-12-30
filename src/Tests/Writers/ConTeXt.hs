83680430b3bda2938177d5a25e917c2e3645197a
John MacFarlane <fiddlosopher@gmail.com>
2011-12-30 13:05:04 -0800

    Updated ConTeXt tests.

--- src/Tests/Writers/ConTeXt.hs
+++ src/Tests/Writers/ConTeXt.hs
@@ -43,9 +43,9 @@ tests = [ testGroup "inline code"
           ]
         , testGroup "headers"
           [ "level 1" =:
-            header 1 "My header" =?> "\\subject{My header}"
+            header 1 "My header" =?> "\\section{My header}"
           , property "header 1 property" $ \ils ->
-                context' (header 1 ils) == "\\subject{" ++ context' ils ++ "}"
+                context' (header 1 ils) == "\\section{" ++ context' ils ++ "}"
           ]
         , testGroup "bullet lists"
           [ "nested" =:
