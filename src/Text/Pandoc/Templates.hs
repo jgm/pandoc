dafd2e555f3e954dfb6e1395bce498221134ccd2
John MacFarlane <fiddlosopher@gmail.com>
2012-01-19 23:45:39 -0800

    Templates: Removed vestigial remnant of openxml template.
    Closes #381 (I think).

--- src/Text/Pandoc/Templates.hs
+++ src/Text/Pandoc/Templates.hs
@@ -83,8 +83,8 @@ getDefaultTemplate :: (Maybe FilePath) -- ^ User data directory to search first
                    -> IO (Either E.IOException String)
 getDefaultTemplate _ "native" = return $ Right ""
 getDefaultTemplate _ "json"   = return $ Right ""
+getDefaultTemplate _ "docx"   = return $ Right ""
 getDefaultTemplate user "odt" = getDefaultTemplate user "opendocument"
-getDefaultTemplate user "docx" = getDefaultTemplate user "openxml"
 getDefaultTemplate user "epub" = getDefaultTemplate user "html"
 getDefaultTemplate user "beamer" = getDefaultTemplate user "latex"
 getDefaultTemplate user writer = do
