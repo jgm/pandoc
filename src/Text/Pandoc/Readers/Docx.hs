a94a7240087b2fbf82a5a6468d74ebaa85bb3849
Wenlong Chen <qzchenwl@gmail.com>
2012-02-15 21:44:48 +0800

    extract files from docx

--- src/Text/Pandoc/Readers/Docx.hs
+++ src/Text/Pandoc/Readers/Docx.hs
@@ -1,11 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Text.Pandoc.Readers.Docx (readDocx) where
 
 import Text.Pandoc.Parsing
 import Text.Pandoc.Definition
 import Data.ByteString.Lazy.Char8 (ByteString)
+import Data.ByteString.Lazy.UTF8 (toString)
+import Codec.Archive.Zip (toArchive, findEntryByPath, fromEntry)
 
 -- | Convert OpenXML-formatted string to 'Pandoc' document.
 readDocx :: ParserState   -- ^ Parser state
          -> ByteString    -- ^ ByteString to parse
          -> Pandoc
-readDocx _ _ = Pandoc (Meta [] [] []) [Para [Str "hello world"]]
+readDocx _ bs = Pandoc (Meta [] [] []) [Para [Str str]]
+    where
+      str      = (take 100 . toString) document
+      document = readFromZip "word/document.xml" bs
+
+readFromZip :: FilePath -> ByteString -> ByteString
+readFromZip path bs = maybe "" fromEntry (findEntryByPath path $ toArchive bs)
