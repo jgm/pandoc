45005cbae45a0f55dbe338a9a132d5e571b31f1d
Wenlong Chen <qzchenwl@gmail.com>
2012-02-17 20:53:40 +0800

    extract text

--- src/Text/Pandoc/Readers/Docx.hs
+++ src/Text/Pandoc/Readers/Docx.hs
@@ -4,6 +4,7 @@ module Text.Pandoc.Readers.Docx (readDocx) where
 
 import Text.Pandoc.Parsing
 import Text.Pandoc.Definition
+import Text.HTML.TagSoup (parseTags)
 import Data.ByteString.Lazy.Char8 (ByteString)
 import Data.ByteString.Lazy.UTF8 (toString)
 import Codec.Archive.Zip (toArchive, findEntryByPath, fromEntry)
@@ -12,10 +13,28 @@ import Codec.Archive.Zip (toArchive, findEntryByPath, fromEntry)
 readDocx :: ParserState   -- ^ Parser state
          -> ByteString    -- ^ ByteString to parse
          -> Pandoc
-readDocx _ bs = Pandoc (Meta [] [] []) [Para [Str str]]
+readDocx st bs = Pandoc meta blocks
     where
-      str      = (take 100 . toString) document
+      meta = Meta [] [] []
+      blocks = readWith parseDocument st tags
+      str = show $ take 100 tags
+      tags = parseTags $ toString document
       document = readFromZip "word/document.xml" bs
 
 readFromZip :: FilePath -> ByteString -> ByteString
 readFromZip path bs = maybe "" fromEntry (findEntryByPath path $ toArchive bs)
+
+type TagParser = GenParser (Tag String) ParserState
+
+parseDocument :: TagParser [Block]
+parseDocument = try $ do
+  skipMany nonText
+  contents <- many $ (pText >>~ skipMany nonText)
+  return contents
+
+nonText = pSatisfy isTagText
+
+pText :: TagParser Block
+pText = try $ do
+  (TagText str) <- pSatisfy isTagText
+  return (Str str)
