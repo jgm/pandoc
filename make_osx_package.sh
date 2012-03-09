d154aff4fcd496d36e4ded70292464b01149e277
John MacFarlane <fiddlosopher@gmail.com>
2012-03-09 14:23:06 -0800

    make_osx_package:  removed --reinstall flag on citeproc-hs.

--- make_osx_package.sh
+++ make_osx_package.sh
@@ -14,7 +14,7 @@ mkdir -p $RESOURCES
 
 echo Building pandoc...
 sudo cabal-dev install-deps
-sudo cabal-dev install --reinstall --flags="embed_data_files" citeproc-hs
+sudo cabal-dev install --flags="embed_data_files" citeproc-hs
 sudo cabal-dev install --disable-library-for-ghci highlighting-kate
 sudo cabal-dev install --prefix=/usr/local --datasubdir=$BASE --docdir=/usr/local/doc/$BASE --flags="executable -library"
 sudo cabal-dev copy --destdir=$ROOT
