c7bd034fa8359e2c3f584cfc23cd1fd8deaab8c0
John MacFarlane <jgm@berkeley.edu>
2012-08-16 19:57:03 -0700

    Added comments in Options.

--- src/Text/Pandoc/Options.hs
+++ src/Text/Pandoc/Options.hs
@@ -46,44 +46,46 @@ import Data.Default
 import Text.Pandoc.Highlighting (Style, pygments)
 
 -- | Individually selectable syntax extensions.
-data Extension = Ext_footnotes
-               | Ext_inline_notes
-               | Ext_pandoc_title_block
-               | Ext_mmd_title_block
-               | Ext_table_captions
-               -- | Ext_image_captions
-               | Ext_simple_tables
-               | Ext_multiline_tables
-               | Ext_grid_tables
-               | Ext_pipe_tables
-               | Ext_citations
-               | Ext_raw_tex
-               | Ext_raw_html
-               | Ext_tex_math_dollars
-               | Ext_tex_math_single_backslash
-               | Ext_tex_math_double_backslash
-               | Ext_latex_macros
-               | Ext_delimited_code_blocks
-               | Ext_inline_code_attributes
-               | Ext_markdown_in_html_blocks
-               | Ext_markdown_attribute
-               | Ext_escaped_line_breaks
-               | Ext_autolink_code_spans
-               | Ext_fancy_lists
-               | Ext_startnum
-               | Ext_definition_lists
-               | Ext_example_lists
-               | Ext_all_symbols_escapable
-               | Ext_intraword_underscores
-               | Ext_blank_before_blockquote
-               | Ext_blank_before_header
-               -- | Ext_significant_bullets
-               | Ext_strikeout
-               | Ext_superscript
-               | Ext_subscript
-               | Ext_hard_line_breaks
-               | Ext_literate_haskell
-               deriving (Show, Read, Enum, Eq, Ord, Bounded)
+data Extension =
+      Ext_footnotes           -- ^ Pandoc/PHP/MMD style footnotes
+    | Ext_inline_notes        -- ^ Pandoc-style inline notes
+    | Ext_pandoc_title_block  -- ^ Pandoc title block
+    | Ext_mmd_title_block     -- ^ Multimarkdown metadata block
+    | Ext_table_captions      -- ^ Pandoc-style table captions
+    -- | Ext_image_captions
+    | Ext_simple_tables       -- ^ Pandoc-style simple tables
+    | Ext_multiline_tables    -- ^ Pandoc-style multiline tables
+    | Ext_grid_tables         -- ^ Grid tables (pandoc, reST)
+    | Ext_pipe_tables         -- ^ Pipe tables (as in PHP markdown extra)
+    | Ext_citations           -- ^ Pandoc/citeproc citations
+    | Ext_raw_tex             -- ^ Allow raw TeX (other than math)
+    | Ext_raw_html            -- ^ Allow raw HTML
+    | Ext_tex_math_dollars    -- ^ TeX math between $..$ or $$..$$
+    | Ext_tex_math_single_backslash  -- ^ TeX math btw \(..\) \[..\]
+    | Ext_tex_math_double_backslash  -- ^ TeX math btw \\(..\\) \\[..\\]
+    | Ext_latex_macros        -- ^ Parse LaTeX macro definitions (for math only)
+    | Ext_delimited_code_blocks   -- ^ Parse delimited code blocks
+    | Ext_inline_code_attributes  -- ^ Allow attributes on inline code
+    | Ext_markdown_in_html_blocks -- ^ Interpret as markdown inside HTML blocks
+    | Ext_markdown_attribute      -- ^ Interpret text inside HTML as markdown
+                                  --   iff container has attribute 'markdown'
+    | Ext_escaped_line_breaks     -- ^ Treat a backslash at EOL as linebreak
+    | Ext_autolink_code_spans -- ^ Put autolink text inside code spans
+    | Ext_fancy_lists         -- ^ Enable fancy list numbers and delimiters
+    | Ext_startnum            -- ^ Make start number of ordered list significant
+    | Ext_definition_lists    -- ^ Definition lists as in pandoc, mmd, php
+    | Ext_example_lists       -- ^ Markdown-style numbered examples
+    | Ext_all_symbols_escapable  -- ^ Make all non-alphanumerics escapable
+    | Ext_intraword_underscores  -- ^ Treat underscore inside word as literal
+    | Ext_blank_before_blockquote -- ^ Require blank line before a blockquote
+    | Ext_blank_before_header     -- ^ Require blank line before a header
+    -- | Ext_significant_bullets
+    | Ext_strikeout           -- ^ Strikeout using ~~this~~ syntax
+    | Ext_superscript         -- ^ Superscript using ^this^ syntax
+    | Ext_subscript           -- ^ Subscript using ~this~ syntax
+    | Ext_hard_line_breaks    -- ^ All newlines become hard line breaks
+    | Ext_literate_haskell    -- ^ Enable literate Haskell conventions
+    deriving (Show, Read, Enum, Eq, Ord, Bounded)
 
 pandocExtensions :: Set Extension
 pandocExtensions = Set.fromList
