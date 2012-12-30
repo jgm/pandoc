bce1522f974ccfdd7fe567d8c78f5480e5707074
John MacFarlane <fiddlosopher@gmail.com>
2012-12-29 18:51:41 -0800

    Updated windows installer to use `embed_data_files` flag.

--- windows/make-windows-installer.bat
+++ windows/make-windows-installer.bat
@@ -4,11 +4,9 @@ cabal update
 cabal-dev clean
 cabal-dev install --disable-library-for-ghci highlighting-kate
 cabal-dev install --flags="embed_data_files" citeproc-hs
-cabal-dev install --flags="executable -library blaze_html_0_5" --datasubdir=
-rem note: we use -f-library in building pandoc, because
-rem if the library is built, the data file paths will not be relocatable!
+cabal-dev install --flags="embed_data_files"
 strip cabal-dev\bin\pandoc.exe
-cabal-dev\bin\pandoc.exe -s --template templates\default.html -S README -o README.html
+cabal-dev\bin\pandoc.exe -s --template data\templates\default.html -S README -o README.html
 copy COPYING COPYING.txt
 copy COPYRIGHT COPYRIGHT.txt
 cd windows
