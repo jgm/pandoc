65528e48f8c7bd96e97b09e07a192a877acd173c
John MacFarlane <fiddlosopher@gmail.com>
2013-01-20 19:36:44 -0800

    make-windows-installer:  force reinstall of citeproc-hs and h-k.

--- windows/make-windows-installer.bat
+++ windows/make-windows-installer.bat
@@ -2,8 +2,8 @@
 cd ..
 cabal update
 cabal-dev clean
-cabal-dev install --disable-library-for-ghci highlighting-kate
-cabal-dev install --flags="embed_data_files" citeproc-hs
+cabal-dev install --reinstall --force-reinstall --disable-library-for-ghci highlighting-kate
+cabal-dev install --reinstall --force-reinstall --flags="embed_data_files" citeproc-hs
 cabal-dev install --flags="embed_data_files"
 strip cabal-dev\bin\pandoc.exe
 cabal-dev\bin\pandoc.exe -s --template data\templates\default.html -S README -o README.html
