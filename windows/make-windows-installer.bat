a5da6898c9f19af89be89cf5b83e1006201678d4
John MacFarlane <fiddlosopher@gmail.com>
2013-01-22 19:43:32 -0800

    Changes to make-windows-installer.bat.
    * Exit batch file if any of the cabal-dev installs fail.
    * There's no longer any need to reinstall highlighting-kate.
    * Don't start with a 'cabal update'; leave that to the user.

--- windows/make-windows-installer.bat
+++ windows/make-windows-installer.bat
@@ -1,12 +1,14 @@
 @echo off
 cd ..
-cabal update
+rem cabal update
 cabal-dev clean
-cabal-dev install --reinstall --force-reinstall highlighting-kate
 cabal-dev install --reinstall --force-reinstall --flags="embed_data_files" citeproc-hs
+if %errorlevel% neq 0 exit /b %errorlevel%
 cabal-dev install --reinstall --force-reinstall --flags="embed_data_files"
+if %errorlevel% neq 0 exit /b %errorlevel%
 strip cabal-dev\bin\pandoc.exe
 cabal-dev\bin\pandoc.exe -s --template data\templates\default.html -S README -o README.html
+if %errorlevel% neq 0 exit /b %errorlevel%
 copy COPYING COPYING.txt
 copy COPYRIGHT COPYRIGHT.txt
 cd windows
