ce4b7fc416ebe14fc1f51a6860e15a4d999b9803
John MacFarlane <fiddlosopher@gmail.com>
2013-01-25 19:53:09 -0800

    Changed copyright date range on Markdown reader.

--- src/Text/Pandoc/Readers/Markdown.hs
+++ src/Text/Pandoc/Readers/Markdown.hs
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 {- |
    Module      : Text.Pandoc.Readers.Markdown
-   Copyright   : Copyright (C) 2006-2010 John MacFarlane
+   Copyright   : Copyright (C) 2006-2013 John MacFarlane
    License     : GNU GPL, version 2 or above
 
    Maintainer  : John MacFarlane <jgm@berkeley.edu>
