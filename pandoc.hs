72c445594e214a794b7cb4cc14d0e26998b3c5aa
John MacFarlane <fiddlosopher@gmail.com>
2013-03-17 17:43:51 -0700

    Added Text.Pandoc.Readers.OPML, exporting readOPML.
    The _note attribute is supported.  This is unofficial, but
    used e.g. in OmniOutliner and supported by multimarkdown.
    We treat the contents as markdown blocks under a section
    header.
    
    Added to documentation and tests.

--- pandoc.hs
+++ pandoc.hs
@@ -767,6 +767,7 @@ defaultReaderName fallback (x:xs) =
     ".rst"      -> "rst"
     ".lhs"      -> "markdown+lhs"
     ".db"       -> "docbook"
+    ".opml"     -> "opml"
     ".wiki"     -> "mediawiki"
     ".textile"  -> "textile"
     ".native"   -> "native"
