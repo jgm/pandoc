883f119c871a9fce402ceace8492a3cef06c5e81
claremacrae <github@cfmacrae.fastmail.co.uk>
2013-08-19 08:09:52 +0100

    Removed unnecessary line-break after hard break in dokuwiki writer (#386)

--- src/Text/Pandoc/Writers/DokuWiki.hs
+++ src/Text/Pandoc/Writers/DokuWiki.hs
@@ -431,7 +431,7 @@ inlineToDokuWiki _ (RawInline "mediawiki" str) = return str
 inlineToDokuWiki _ (RawInline "html" str) = return str
 inlineToDokuWiki _ (RawInline _ _) = return ""
 
-inlineToDokuWiki _ (LineBreak) = return "\\\\\n"
+inlineToDokuWiki _ (LineBreak) = return "\\\\ "
 
 inlineToDokuWiki _ Space = return " "
 
