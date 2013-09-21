b1509e90dff45daeb72c34c5858bd186913c927d
John MacFarlane <fiddlosopher@gmail.com>
2013-09-20 17:37:19 -0700

    make_osx_package:  small code cleanup.

--- make_osx_package.sh
+++ make_osx_package.sh
@@ -5,6 +5,7 @@ SANDBOX=`pwd`/.cabal-sandbox
 VERSION=$(grep -e '^Version' pandoc.cabal | awk '{print $2}')
 RESOURCES=$DIST/Resources
 ROOT=$DIST/pandoc
+DEST=$ROOT/usr/local
 SCRIPTS=osx-resources
 BASE=pandoc-$VERSION
 ME=jgm
@@ -24,29 +25,28 @@ cabal sandbox init
 cabal install --reinstall --flags="embed_data_files" pandoc-citeproc
 cabal install --reinstall --flags="embed_data_files"
 
-mkdir -p $ROOT/usr/local/bin
-mkdir -p $ROOT/usr/local/share/man/man1
-mkdir -p $ROOT/usr/local/share/man/man5
-mkdir -p $ROOT/usr/local/opt/icu4c/lib
+mkdir -p $DEST/bin
+mkdir -p $DEST/share/man/man1
+mkdir -p $DEST/share/man/man5
 for f in $EXES; do
-  cp $SANDBOX/bin/$f $ROOT/usr/local/bin/;
-  cp $SANDBOX/share/man/man1/$f.1 $ROOT/usr/local/share/man/man1/
+  cp $SANDBOX/bin/$f $DEST/bin/;
+  cp $SANDBOX/share/man/man1/$f.1 $DEST/share/man/man1/
 done
-cp $SANDBOX/share/man/man5/pandoc_markdown.5 $ROOT/usr/local/share/man/man5/
+cp $SANDBOX/share/man/man5/pandoc_markdown.5 $DEST/share/man/man5/
 
 chown -R $ME:staff $DIST
-# gzip $ROOT/usr/local/share/man/man?/*.*
+# gzip $DEST/share/man/man?/*.*
 # cabal gives man pages the wrong permissions
-chmod +r $ROOT/usr/local/share/man/man?/*.*
+chmod +r $DEST/share/man/man?/*.*
 
 echo Copying license...
 $SANDBOX/bin/pandoc --data data -t rtf -s COPYING -o $RESOURCES/License.rtf
 
 echo Signing pandoc executable...
 
-codesign --force --sign "$CODESIGNID" $ROOT/usr/local/bin/pandoc
+codesign --force --sign "$CODESIGNID" $DEST/bin/pandoc
 # make sure it's valid... returns nonzero exit code if it isn't:
-spctl --assess --type execute $ROOT/usr/local/bin/pandoc
+spctl --assess --type execute $DEST/bin/pandoc
 
 echo Creating OSX package...
 
