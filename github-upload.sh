4f64a4bc3bbd7e976635bcf5752633895a5aa3d8
John MacFarlane <fiddlosopher@gmail.com>
2013-12-07 12:59:08 -0800

    More changes to github-upload.sh.  Still doesn't work, though.

--- github-upload.sh
+++ github-upload.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 VERSION=$1
 FULLNAME=pandoc-$VERSION
@@ -8,11 +8,11 @@ curl -H "Authorization: token $TOKEN" \
      -H "Accept: application/vnd.github.manifold-preview" \
      -H "Content-Type: application/x-apple-diskimage" \
      --data-binary @$FULLNAME.dmg \
-     "https://uploads.github.com/repos/jgm/pandoc/releases/$FULLNAME/assets?name=$FULLNAME.dmg"
+     "https://uploads.github.com/repos/jgm/pandoc/releases/$VERSION/assets?name=$FULLNAME.dmg"
 
 curl -H "Authorization: token $TOKEN" \
      -H "Accept: application/vnd.github.manifold-preview" \
      -H "Content-Type: application/x-msi" \
      --data-binary @$FULLNAME.msi \
-     "https://uploads.github.com/repos/jgm/pandoc/releases/$FULLNAME/assets?name=$FULLNAME.msi"
+     "https://uploads.github.com/repos/jgm/pandoc/releases/$VERSION/assets?name=$FULLNAME.msi"
 
