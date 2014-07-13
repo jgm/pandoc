3516d9d09b8ac0d77404b80ebf4a4236227d50cc
John MacFarlane <jgm@berkeley.edu>
2014-07-12 23:47:38 -0700

    Fixed typo in module header for Asciify.
    Thanks to @dubiousjim. Closes #1419.

--- src/Text/Pandoc/Asciify.hs
+++ src/Text/Pandoc/Asciify.hs
@@ -17,7 +17,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 -}
 
 {- |
-   Module      : Text.Pandoc.SelfContained
+   Module      : Text.Pandoc.Asciify
    Copyright   : Copyright (C) 2013-2014 John MacFarlane
    License     : GNU GPL, version 2 or above
 
