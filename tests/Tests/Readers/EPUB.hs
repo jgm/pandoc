c0b9ad4c5df0dd17612d0bd5d82b628a971afcff
mpickering <matthewtpickering@gmail.com>
2014-09-25 13:33:25 +0100

    EPUB Tests: Seperating image testing from other features

--- tests/Tests/Readers/EPUB.hs
+++ tests/Tests/Readers/EPUB.hs
@@ -8,6 +8,7 @@ import qualified Data.ByteString.Lazy as BL
 import Text.Pandoc.Readers.EPUB
 import Text.Pandoc.MediaBag (MediaBag, mediaDirectory)
 import Control.Applicative
+import System.FilePath (joinPath)
 
 getMediaBag :: FilePath -> IO MediaBag
 getMediaBag fp = snd . readEPUB def <$> BL.readFile fp
@@ -22,12 +23,12 @@ testMediaBag fp bag = do
              (actBag == bag)
 
 featuresBag :: [(String, String, Int)]
-featuresBag = [("img/ElementaryMathExample.png","image/png",1331),("img/Maghreb1.png","image/png",2520),("img/check.gif","image/gif",1340),("img/check.jpg","image/jpeg",2661),("img/check.png","image/png",2815),("img/cichons_diagram.png","image/png",7045),("img/complex_number.png","image/png",5238),("img/multiscripts_and_greek_alphabet.png","image/png",10060)]
+featuresBag = [(joinPath ["img","check.gif"],"image/gif",1340),(joinPath ["img","check.jpg"],"image/jpeg",2661),(joinPath ["img","check.png"],"image/png",2815),(joinPath ["img","multiscripts_and_greek_alphabet.png"],"image/png",10060)]
 
 tests :: [Test]
 tests =
   [ testGroup "EPUB Mediabag"
     [ testCase "features bag"
-      (testMediaBag "epub/features.epub" featuresBag)
+      (testMediaBag "epub/img.epub" featuresBag)
     ]
   ]
