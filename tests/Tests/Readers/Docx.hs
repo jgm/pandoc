60846471a3dfd5d77270d832f41954ffc9696b3c
Jesse Rosenthal <jrosenthal@jhu.edu>
2014-10-25 16:02:31 -0400

    Docx test: Remove Danish header test.
    Redundant, now that we're testing for a more generalized sort of
    internationalized blocks.

--- tests/Tests/Readers/Docx.hs
+++ tests/Tests/Readers/Docx.hs
@@ -156,10 +156,6 @@ tests = [ testGroup "inlines"
             "docx/numbered_header.docx"
             "docx/numbered_header.native"
           , testCompare
-            "headers in other languages"
-            "docx/danish_headers.docx"
-            "docx/danish_headers.native"
-          , testCompare
             "i18n blocks (headers and blockquotes)"
             "docx/i18n_blocks.docx"
             "docx/i18n_blocks.native"
