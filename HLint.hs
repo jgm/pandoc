f498221211fd04718f161224364a84ebbbd45a95
Grégory Bataille <gbataille.dev@gmail.com>
2015-04-18 16:02:33 +0200

    Parser structure

---
+++ HLint.hs
@@ -0,0 +1,1 @@
+ignore "Redundant bracket"
--- src/Text/Pandoc/Readers/AsciiDoc.hs
+++ src/Text/Pandoc/Readers/AsciiDoc.hs
@@ -3,10 +3,23 @@ module Text.Pandoc.Readers.AsciiDoc (
 ) where
 
 import qualified Data.Map as Map
+import qualified Data.Sequence as Seq
 import Text.Pandoc.Definition
 import Text.Pandoc.Options
+import Text.Pandoc.Parsing
+import qualified Text.Pandoc.Builder as B
+
+type AsciiDocParser = Parser String ParserState
 
 readAsciiDoc :: ReaderOptions
              -> String
              -> Pandoc
-readAsciiDoc opts s = Pandoc (Meta (Map.singleton "foo" (MetaString "bar"))) []
+-- readAsciiDoc opts s = Pandoc (Meta (Map.singleton "foo" (MetaString "bar"))) []
+readAsciiDoc opts s =
+  (readWith parseAsciiDoc) def{ stateOptions = opts } (s ++ "\n\n")
+
+parseAsciiDoc :: AsciiDocParser Pandoc
+parseAsciiDoc = do
+  -- markdown allows raw HTML
+  let meta = (Meta (Map.singleton "foo" (MetaString "bar")))
+  return $ B.doc (B.Many Seq.empty)
