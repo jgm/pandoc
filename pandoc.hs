c6debff1f898b0fd6a36efe8397dbfd18b84cacf
John MacFarlane <jgm@berkeley.edu>
2015-05-11 21:19:15 -0700

    Removed references to `biblio-files` in pandoc.hs and README.

--- pandoc.hs
+++ pandoc.hs
@@ -751,9 +751,6 @@ options =
                   (\arg opt -> return opt{ optMetadata = addMetadata
                                              "bibliography" (readMetaValue arg)
                                              $ optMetadata opt
-                                         , optVariables =
-                                            ("biblio-files", dropExtension arg) :
-                                            optVariables opt
                                          })
                    "FILE")
                  ""
