b8634b9f75eaa8892a58ffd0cbe0ff8d89c257c8
John MacFarlane <jgm@berkeley.edu>
2015-07-13 22:50:17 -0700

    HTML writer:  support speaker notes in dzslides.
    With this change `<div class="notes">` and also `<div class="notes"
    role="note">` will be output if `-t dzslides` is used. So we can
    have speaker notes in dzslides too.
    
    Thanks to maybegeek.

--- src/Text/Pandoc/Writers/HTML.hs
+++ src/Text/Pandoc/Writers/HTML.hs
@@ -458,6 +458,7 @@ blockToHtml opts (Div attr@(_,classes,_) bs) = do
      if speakerNotes
         then case writerSlideVariant opts of
                   RevealJsSlides -> addAttrs opts' attr $ H5.aside $ contents'
+                  DZSlides       -> addAttrs opts' attr $ H5.div $ contents'
                   NoSlides       -> addAttrs opts' attr $ H.div $ contents'
                   _              -> mempty
         else addAttrs opts attr $ H.div $ contents'
