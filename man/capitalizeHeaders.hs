7120df9dad6376a6a43b7e870350a016a720a53c
John MacFarlane <jgm@berkeley.edu>
2015-07-21 10:24:59 -0700

    Don't capitalize header links in man page.
    Also regenerated man page from latest README.

--- man/capitalizeHeaders.hs
+++ man/capitalizeHeaders.hs
@@ -7,12 +7,14 @@ main = toJSONFilter capitalizeHeaders
 
 capitalizeHeaders :: Block -> Block
 capitalizeHeaders (Header 1 attr xs) = Header 1 attr $ walk capitalize xs
-capitalizeHeaders x = walk capitalizeHeaderLinks x
+capitalizeHeaders x = x
 
 capitalize :: Inline -> Inline
 capitalize (Str xs) = Str $ map toUpper xs
 capitalize x = x
 
+{-
 capitalizeHeaderLinks :: Inline -> Inline
 capitalizeHeaderLinks (Link xs t@('#':_,_)) = Link (walk capitalize xs) t
 capitalizeHeaderLinks x = x
+-}
