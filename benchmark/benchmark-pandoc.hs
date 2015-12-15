3e005aa413fdedb39f434015954c33721ff9c001
John MacFarlane <jgm@berkeley.edu>
2015-12-14 20:34:53 -0800

    Fixed benchmark for new API.

--- benchmark/benchmark-pandoc.hs
+++ benchmark/benchmark-pandoc.hs
@@ -28,7 +28,7 @@ readerBench :: Pandoc
 readerBench doc (name, reader) =
   case lookup name writers of
        Just (PureStringWriter writer) ->
-         let inp = writer def{ writerWrapText = True} doc
+         let inp = writer def{ writerWrapText = WrapAuto} doc
          in return $ bench (name ++ " reader") $ nfIO $
                  (fmap handleError <$> reader def{ readerSmart = True }) inp
        _ -> trace ("\nCould not find writer for " ++ name ++ "\n") Nothing
@@ -37,7 +37,7 @@ writerBench :: Pandoc
             -> (String, WriterOptions -> Pandoc -> String)
             -> Benchmark
 writerBench doc (name, writer) = bench (name ++ " writer") $ nf
-    (writer def{ writerWrapText = True }) doc
+    (writer def{ writerWrapText = WrapAuto }) doc
 
 main :: IO ()
 main = do
