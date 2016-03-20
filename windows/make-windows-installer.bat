c7df80e0c3116ad05bc107a6951f82786d273209
John MacFarlane <jgm@berkeley.edu>
2016-03-20 09:01:52 -0700

    Removed strip commands from make-windows-installer.bat.

--- windows/make-windows-installer.bat
+++ windows/make-windows-installer.bat
@@ -2,8 +2,6 @@
 stack install --test
 if %errorlevel% neq 0 exit /b %errorlevel%
 for /f "delims=" %%a in ('stack path --local-bin-path') do @set BINPATH=%%a
-strip %BINPATH%\pandoc.exe
-strip %BINPATH%\pandoc-citeproc.exe
 %BINPATH%\pandoc.exe -s -S ..\README -o README.html
 if %errorlevel% neq 0 exit /b %errorlevel%
 %BINPATH%\pandoc.exe -s ..\COPYING -t rtf -S -o COPYING.rtf
