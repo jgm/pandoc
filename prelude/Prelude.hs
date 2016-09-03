09d96bb88492a8a26a7ed22c40b044b1a63d520d
Jesse Rosenthal <jrosenthal@jhu.edu>
2016-09-03 15:23:32 -0400

    Remove unnecessary CPP in custom Prelude.
    We don't have to worry about base < 4.7.

--- prelude/Prelude.hs
+++ prelude/Prelude.hs
@@ -19,12 +19,8 @@ where
 
 #if MIN_VERSION_base(4,8,0)
 import "base" Prelude as P
-#elif MIN_VERSION_base(4,6,0)
-import "base" Prelude as P
-import Control.Applicative
-import Data.Monoid
 #else
-import "base" Prelude as P hiding (catch)
+import "base" Prelude as P
 import Control.Applicative
 import Data.Monoid
 #endif
