df1ca2b1a55a6dca762651028cd12a60cd426579
Jesse Rosenthal <jrosenthal@jhu.edu>
2016-09-03 08:10:53 -0400

    Add EOL note to time compat module.
    Because time 1.4 is a boot library for GHC 7.8, we will support the
    compatibility module as long as we support 7.8. But we should be clear
    about when we will no longer need it.

--- src/Text/Pandoc/Compat/Time.hs
+++ src/Text/Pandoc/Compat/Time.hs
@@ -1,4 +1,16 @@
 {-# LANGUAGE CPP #-}
+
+{-
+This compatibility module is needed because, in time 1.5, the
+`defaultTimeLocale` function was moved from System.Locale (in the
+old-locale library) into Data.Time.
+
+We support both behaviors because time 1.4 is a boot library for GHC
+7.8. time 1.5 is a boot library for GHC 7.10.
+
+When support is dropped for GHC 7.8, this module may be obsoleted.
+-}
+
 #if MIN_VERSION_time(1,5,0)
 module Text.Pandoc.Compat.Time (
   module Data.Time
