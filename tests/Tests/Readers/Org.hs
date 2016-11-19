jgm/pandoc

commit 7e5220b57c5a48fabe6e43ba270db812593d3463

Org reader: allow HTML attribs on non-figure images
Images which are the only element in a paragraph can still be given HTML
attributes, even if the image does not have a caption and is hence not a figure.
The following will add set the `width` attribute of the image to `50%`:

    #+ATTR_HTML: :width 50%
    [[file:image.jpg]]

Closes: #3222

src/Text/Pandoc/Readers/Org/Blocks.hs
@@ -618,7 +618,8 @@ propertiesDrawer = try $ do
 -- Figures
 --
 
--- | Figures (Image on a line by itself, preceded by name and/or caption)
+-- | Figures or an image paragraph (i.e. an image on a line by itself). Only
+-- images with a caption attribute are interpreted as figures.
 figure :: OrgParser (F Blocks)
 figure = try $ do
   figAttrs <- blockAttributes
@@ -626,23 +627,24 @@ figure = try $ do
   case cleanLinkString src of
     Nothing     -> mzero
     Just imgSrc -> do
-      guard (not . isNothing . blockAttrCaption $ figAttrs)
       guard (isImageFilename imgSrc)
-      return $ figureBlock figAttrs imgSrc
+      let isFigure = not . isNothing $ blockAttrCaption figAttrs
+      return $ imageBlock isFigure figAttrs imgSrc
  where
    selfTarget :: OrgParser String
    selfTarget = try $ char '[' *> linkTarget <* char ']'
 
-   figureBlock :: BlockAttributes -> String -> (F Blocks)
-   figureBlock figAttrs imgSrc =
+   imageBlock :: Bool -> BlockAttributes -> String -> F Blocks
+   imageBlock isFigure figAttrs imgSrc =
      let
        figName    = fromMaybe mempty $ blockAttrName figAttrs
        figLabel   = fromMaybe mempty $ blockAttrLabel figAttrs
        figCaption = fromMaybe mempty $ blockAttrCaption figAttrs
        figKeyVals = blockAttrKeyValues figAttrs
        attr       = (figLabel, mempty, figKeyVals)
+       figTitle   = (if isFigure then withFigPrefix else id) figName
      in
-       B.para . B.imageWith attr imgSrc (withFigPrefix figName) <$> figCaption
+       B.para . B.imageWith attr imgSrc figTitle <$> figCaption
 
    withFigPrefix :: String -> String
    withFigPrefix cs =

tests/Tests/Readers/Org.hs
@@ -196,6 +196,12 @@ tests =
           "[[file:sunrise.jpg]]" =?>
           (para $ image "sunrise.jpg" "" "")
 
+      , "Image with html attributes" =:
+          unlines [ "#+ATTR_HTML: :width 50%"
+                  , "[[file:guinea-pig.gif]]"
+                  ] =?>
+          (para $ imageWith ("", [], [("width", "50%")]) "guinea-pig.gif" "" "")
+
       , "Explicit link" =:
           "[[http://zeitlens.com/][pseudo-random /nonsense/]]" =?>
           (para $ link "http://zeitlens.com/" ""

commit 1a8af5fc442287ef2f7775c4de179e4520487a4f

Org reader: Ensure images in paragraphs are not parsed as figures
This fixes a regression introduced in
7e5220b57c5a48fabe6e43ba270db812593d3463.

src/Text/Pandoc/Readers/Org/BlockStarts.hs
@@ -37,9 +37,11 @@ module Text.Pandoc.Readers.Org.BlockStarts
   , commentLineStart
   , bulletListStart
   , orderedListStart
+  , endOfBlock
   ) where
 
-import           Text.Pandoc.Readers.Org.Parsing
+import Control.Monad ( void )
+import Text.Pandoc.Readers.Org.Parsing
 
 -- | Horizontal Line (five -- dashes or more)
 hline :: OrgParser ()
@@ -110,3 +112,25 @@ noteMarker = try $ do
          , (++) <$> string "fn:"
                 <*> many1Till (noneOf "\n\r\t ") (char ']')
          ]
+
+  -- | Succeeds if the parser is at the end of a block.
+endOfBlock :: OrgParser ()
+endOfBlock = lookAhead . try $ do
+    void blankline <|> anyBlockStart <|> void noteMarker
+ where
+   -- | Succeeds if there is a new block starting at this position.
+   anyBlockStart :: OrgParser ()
+   anyBlockStart = try . choice $
+     [ exampleLineStart
+     , hline
+     , metaLineStart
+     , commentLineStart
+     , void noteMarker
+     , void tableStart
+     , void drawerStart
+     , void headerStart
+     , void latexEnvStart
+     , void bulletListStart
+     , void orderedListStart
+     ]
+

src/Text/Pandoc/Readers/Org/Blocks.hs
@@ -623,7 +623,7 @@ propertiesDrawer = try $ do
 figure :: OrgParser (F Blocks)
 figure = try $ do
   figAttrs <- blockAttributes
-  src <- skipSpaces *> selfTarget <* skipSpaces <* newline
+  src <- skipSpaces *> selfTarget <* skipSpaces <* endOfParagraph
   case cleanLinkString src of
     Nothing     -> mzero
     Just imgSrc -> do
@@ -651,6 +651,10 @@ figure = try $ do
      if "fig:" `isPrefixOf` cs
      then cs
      else "fig:" ++ cs
+
+-- | Succeeds if looking at the end of the current paragraph
+endOfParagraph :: OrgParser ()
+endOfParagraph = try $ skipSpaces *> newline *> endOfBlock
 
 
 --

src/Text/Pandoc/Readers/Org/Inlines.hs
@@ -33,7 +33,7 @@ module Text.Pandoc.Readers.Org.Inlines
   , linkTarget
   ) where
 
-import           Text.Pandoc.Readers.Org.BlockStarts
+import           Text.Pandoc.Readers.Org.BlockStarts ( endOfBlock, noteMarker )
 import           Text.Pandoc.Readers.Org.ParserState
 import           Text.Pandoc.Readers.Org.Parsing
 import           Text.Pandoc.Readers.Org.Shared
@@ -152,18 +152,7 @@ str = return . B.str <$> many1 (noneOf $ specialChars ++ "\n\r ")
 endline :: OrgParser (F Inlines)
 endline = try $ do
   newline
-  notFollowedBy blankline
-  notFollowedBy' exampleLineStart
-  notFollowedBy' hline
-  notFollowedBy' noteMarker
-  notFollowedBy' tableStart
-  notFollowedBy' drawerStart
-  notFollowedBy' headerStart
-  notFollowedBy' metaLineStart
-  notFollowedBy' latexEnvStart
-  notFollowedBy' commentLineStart
-  notFollowedBy' bulletListStart
-  notFollowedBy' orderedListStart
+  notFollowedBy' endOfBlock
   decEmphasisNewlinesCount
   guard =<< newlinesCountWithinLimits
   updateLastPreCharPos

tests/Tests/Readers/Org.hs
@@ -188,19 +188,29 @@ tests =
           "=is_subst = True=" =?>
           para (code "is_subst = True")
 
-      , "Image" =:
-          "[[./sunset.jpg]]" =?>
-          (para $ image "./sunset.jpg" "" "")
+      , testGroup "Images"
+        [ "Image" =:
+            "[[./sunset.jpg]]" =?>
+            (para $ image "./sunset.jpg" "" "")
 
-      , "Image with explicit file: prefix" =:
-          "[[file:sunrise.jpg]]" =?>
-          (para $ image "sunrise.jpg" "" "")
+        , "Image with explicit file: prefix" =:
+            "[[file:sunrise.jpg]]" =?>
+            (para $ image "sunrise.jpg" "" "")
 
-      , "Image with html attributes" =:
-          unlines [ "#+ATTR_HTML: :width 50%"
-                  , "[[file:guinea-pig.gif]]"
-                  ] =?>
-          (para $ imageWith ("", [], [("width", "50%")]) "guinea-pig.gif" "" "")
+        , "Multiple images within a paragraph" =:
+            unlines [ "[[file:sunrise.jpg]]"
+                    , "[[file:sunset.jpg]]"
+                    ] =?>
+            (para $ (image "sunrise.jpg" "" "")
+                 <> softbreak
+                 <> (image "sunset.jpg" "" ""))
+
+        , "Image with html attributes" =:
+            unlines [ "#+ATTR_HTML: :width 50%"
+                    , "[[file:guinea-pig.gif]]"
+                    ] =?>
+            (para $ imageWith ("", [], [("width", "50%")]) "guinea-pig.gif" "" "")
+        ]
 
       , "Explicit link" =:
           "[[http://zeitlens.com/][pseudo-random /nonsense/]]" =?>