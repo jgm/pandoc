5449e4a22617ca28173cde5c31f46c15b1d8bc7f
John MacFarlane <jgm@berkeley.edu>
2016-11-22 22:21:07 +0100

    Docx writer: Give full detail when there are errors converting tex math.

--- src/Text/Pandoc/Writers/Docx.hs
+++ src/Text/Pandoc/Writers/Docx.hs
@@ -1116,7 +1116,8 @@ inlineToOpenXML' opts (Math mathType str) = do
   case writeOMML displayType <$> readTeX str of
         Right r -> return [r]
         Left  e -> do
-          warn $ "Cannot convert the following TeX math, skipping:\n" ++ str
+          warn $ "Cannot convert the following TeX math, skipping:\n" ++ str ++
+                 "\n" ++ e
           inlinesToOpenXML opts (texMathToInlines mathType str)
 inlineToOpenXML' opts (Cite _ lst) = inlinesToOpenXML opts lst
 inlineToOpenXML' opts (Code attrs str) = do
