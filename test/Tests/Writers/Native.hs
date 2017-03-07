93a9c09b6b7ef2b8e8fcf2c6ebbf04bcbfddea3c
John MacFarlane <jgm@berkeley.edu>
2017-03-07 15:14:36 +0100

    Speed up Native writer quickcheck tests.

--- test/Tests/Writers/Native.hs
+++ test/Tests/Writers/Native.hs
@@ -4,17 +4,19 @@ import Test.Framework
 import Tests.Helpers
 import Text.Pandoc
 import Text.Pandoc.Arbitrary ()
+import Test.QuickCheck (mapSize)
 
 p_write_rt :: Pandoc -> Bool
 p_write_rt d =
   read (purely (writeNative def{ writerTemplate = Just "" }) d) == d
 
 p_write_blocks_rt :: [Block] -> Bool
-p_write_blocks_rt bs = length bs > 20 ||
+p_write_blocks_rt bs =
   read (purely (writeNative def) (Pandoc nullMeta bs)) ==
   bs
 
 tests :: [Test]
 tests = [ property "p_write_rt" p_write_rt
-        , property "p_write_blocks_rt" p_write_blocks_rt
+        , property "p_write_blocks_rt" $ mapSize
+             (\x -> if x > 3 then 3 else x) $ p_write_blocks_rt
         ]
