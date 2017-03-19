74fd00b5f6a776df0fd10d3f4bea58d10ae3a2a3
John MacFarlane <jgm@berkeley.edu>
2017-03-19 16:24:52 +0100

    Improved linux tar.gz creation and added Dockerfile to repository.

---
+++ linux/make_tarball.sh
@@ -0,0 +1,15 @@
+set -e
+
+VERSION=$(grep -e '^Version' pandoc.cabal | awk '{print $2}')
+TARGET=pandoc-$VERSION
+
+cd /artifacts
+mkdir $TARGET
+mkdir $TARGET/bin $TARGET/man $TARGET/man/man1
+./pandoc-citeproc --man > $TARGET/man/man1/pandoc-citeproc.1
+cp /usr/src/pandoc/man/pandoc.1 $TARGET/man/man1
+mv pandoc pandoc-citeproc $TARGET/bin
+gzip -9 $TARGET/man/man1/pandoc.1
+gzip -9 $TARGET/man/man1/pandoc-citeproc.1
+tar cvzf $TARGET.tar.gz $TARGET
+rm -r $TARGET
