453970c6b34ed7cc2f52181e5e77b9182a3639e9
John MacFarlane <jgm@berkeley.edu>
2017-03-26 19:19:54 +0200

    Text.Pandoc.Data:  ensure it compiles even without embed_data_files.
    In this case we don't depend on file-embed or use TH.

--- src/Text/Pandoc/Data.hs
+++ src/Text/Pandoc/Data.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE CPP #-}
+#ifdef EMBED_DATA_FILES
 {-# LANGUAGE TemplateHaskell #-}
-
+#endif
 module Text.Pandoc.Data (dataFiles) where
 
-import Data.FileEmbed
+import System.FilePath (FilePath, splitDirectories)
 import qualified Data.ByteString as B
-import System.FilePath (splitDirectories)
 import qualified System.FilePath.Posix as Posix
+#ifdef EMBED_DATA_FILES
+import Data.FileEmbed
+#endif
 
 -- We ensure that the data files are stored using Posix
 -- path separators (/), even on Windows.
@@ -14,4 +18,8 @@ dataFiles = map (\(fp, contents) ->
   (Posix.joinPath (splitDirectories fp), contents)) dataFiles'
 
 dataFiles' :: [(FilePath, B.ByteString)]
+#ifdef EMBED_DATA_FILES
 dataFiles' = ("MANUAL.txt", $(embedFile "MANUAL.txt")) : $(embedDir "data")
+#else
+dataFiles' = error "dataFiles is only defined when embed_data_files flag set"
+#endif
