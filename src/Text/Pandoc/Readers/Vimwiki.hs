9aaea5aec140163bf5ca199173ca9a4f7f3dc079
Jeshiba <baconp@gmail.com>
2017-05-23 16:13:02 -0400

    wrote test function for inlines and blocks

--- src/Text/Pandoc/Readers/Vimwiki.hs
+++ src/Text/Pandoc/Readers/Vimwiki.hs
@@ -1,30 +1,37 @@
 import Control.Monad.Except (throwError)
 import Control.Monad (guard)
 import Data.Default -- def is there
+import Data.Functor.Identity
 import Text.Pandoc.Builder (Blocks, Inlines, trimInlines)
 import qualified Text.Pandoc.Builder as B (doc, toList, headerWith, str, space)
 import Text.Pandoc.Class (PandocMonad, report, PandocIO, runIO)
 import Text.Pandoc.Definition (Pandoc, nullAttr)
+import Text.Pandoc.Error (PandocError)
 import Text.Pandoc.Logging (LogMessage(ParsingTrace))
 import Text.Pandoc.Options (ReaderOptions)
 import Text.Pandoc.Parsing (readWithM, ParserT, stateOptions, ParserState, blanklines, registerHeader, spaceChar)
 import Text.Parsec.Char (spaces, char, anyChar, newline, string, noneOf)
+import Text.Parsec.Error (ParseError)
 import Text.Parsec.Combinator (eof, choice, many1, manyTill, count, skipMany1)
 import Text.Parsec.Pos (sourceColumn)
-import Text.Parsec.Prim (many, getPosition, try)
+import Text.Parsec.Prim (many, getPosition, try, runParserT)
 
 readVimwiki :: PandocMonad m => ReaderOptions -> String -> m Pandoc
 readVimwiki opts s = do
-  res <- readWithM parseVimwiki def{ stateOptions = opts } (s ++ "\n")
+  res <- readWithM parseVimwiki def{ stateOptions = opts } s
   case res of
        Left e -> throwError e
        Right result -> return result
 
 type VwParser = ParserT [Char] ParserState
 
+
 -- constants
 specialChars :: [Char]
 specialChars = "="
+
+spaceChars :: [Char]
+spaceChars = " \t"
 
 -- main parser
 
@@ -68,11 +75,13 @@ guardColumnOne :: PandocMonad m => VwParser m ()
 guardColumnOne = getPosition >>= \pos -> guard (sourceColumn pos == 1)
 
 header = try $ do
-  guardColumnOne
+  many whitespace
   eqs <- many1 (char '=')
+  whitespace
   let lev = length eqs
   guard $ lev <= 6
-  contents <- trimInlines . mconcat <$> manyTill inline (string eqs)
+  contents <- trimInlines . mconcat <$> manyTill inline (try (whitespace >> (string eqs)))
+  --contents <- trimInlines . mconcat <$> manyTill inline (string eqs)
   attr <- registerHeader nullAttr contents
   return $ B.headerWith attr lev contents
 
@@ -119,7 +128,7 @@ inlineMath :: PandocMonad m => VwParser m Inlines
 comment :: PandocMonad m => VwParser m Inlines
 tag :: PandocMonad m => VwParser m Inlines
 
-str = B.str <$> many1 (noneOf $ specialChars)
+str = B.str <$> many1 (noneOf $ specialChars ++ spaceChars)
 whitespace = B.space <$ (skipMany1 spaceChar)
 bareURL  = undefined
 bold  = undefined
@@ -137,3 +146,22 @@ tag  = undefined
 
 -- *Main> runIO (readVimwiki (def :: ReaderOptions) "==2==" :: PandocIO Pandoc)
 -- Right (Pandoc (Meta {unMeta = fromList []}) [Header 2 ("",[],[]) [Str "2"]])
+
+runParser :: VwParser PandocIO a -> String -> PandocIO a
+runParser p s = do
+  res <- readWithM p def{ stateOptions = def :: ReaderOptions } s
+  case res of
+       Left e -> throwError e
+       Right result -> return result
+
+testP :: VwParser PandocIO a -> String -> IO (Either PandocError a)
+testP p s = runIO $ runParser p s
+
+--type Parser = Parsec String ()
+{--runP :: VwParser PandocIO a -> [Char] -> IO (Either PandocError a)
+runP p opts s = do
+  res <- readWithM p def{ stateOptions = opts } s
+  case res of
+       Left e -> throwError e
+       Right result -> return result--}
+--runP p s = runIO (mapLeft (PandocParsecError s) `liftM` runParserT p def{ stateOptions = def :: ReaderOptions } "test" s)
