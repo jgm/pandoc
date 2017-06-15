28c8159a1d3de99e7a4dc33b0e75c43137157e79
Yuchen Pei <baconp@gmail.com>
2017-06-15 09:28:28 -0400

    Revert "updated Readers.hs to agree with upstream/master"
    This reverts commit f9195fbe9ffbd26fe206aeafff383cf7c5409c81.

--- src/Text/Pandoc/Readers.hs
+++ src/Text/Pandoc/Readers.hs
@@ -3,14 +3,17 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-
 Copyright (C) 2006-2017 John MacFarlane <jgm@berkeley.edu>
+
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
+
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -20,13 +23,17 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
    Module      : Text.Pandoc.Readers
    Copyright   : Copyright (C) 2006-2017 John MacFarlane
    License     : GNU GPL, version 2 or above
+
    Maintainer  : John MacFarlane <jgm@berkeley.edu>
    Stability   : alpha
    Portability : portable
+
 This helper module exports the readers.
+
 Note:  all of the readers assume that the input text has @'\n'@
 line endings.  So if you get your input text from a web form,
 you should remove @'\r'@ characters using @filter (/='\r')@.
+
 -}
 
 module Text.Pandoc.Readers
@@ -75,6 +82,7 @@ import Text.Pandoc.Readers.HTML
 import Text.Pandoc.Readers.LaTeX
 import Text.Pandoc.Readers.Markdown
 import Text.Pandoc.Readers.MediaWiki
+import Text.Pandoc.Readers.Vimwiki
 import Text.Pandoc.Readers.Native
 import Text.Pandoc.Readers.Odt
 import Text.Pandoc.Readers.OPML
@@ -83,43 +91,41 @@ import Text.Pandoc.Readers.RST
 import Text.Pandoc.Readers.Textile
 import Text.Pandoc.Readers.TWiki
 import Text.Pandoc.Readers.Txt2Tags
-import Text.Pandoc.Readers.Vimwiki
 import Text.Pandoc.Shared (mapLeft)
 import Text.Parsec.Error
 import qualified Text.Pandoc.UTF8 as UTF8
 import qualified Data.ByteString.Lazy as BL
-import Data.Text (Text)
 
-data Reader m = TextReader (ReaderOptions -> Text -> m Pandoc)
+data Reader m = StringReader (ReaderOptions -> String -> m Pandoc)
               | ByteStringReader (ReaderOptions -> BL.ByteString -> m Pandoc)
 
 -- | Association list of formats and readers.
 readers :: PandocMonad m => [(String, Reader m)]
-readers = [ ("native"       , TextReader readNative)
-           ,("json"         , TextReader $ \o s ->
+readers = [ ("native"       , StringReader readNative)
+           ,("json"         , StringReader $ \o s ->
                                                case readJSON o s of
                                                  Right doc -> return doc
                                                  Left _ -> throwError $ PandocParseError "JSON parse error")
-           ,("markdown"     , TextReader readMarkdown)
-           ,("markdown_strict" , TextReader readMarkdown)
-           ,("markdown_phpextra" , TextReader readMarkdown)
-           ,("markdown_github" , TextReader readMarkdown)
-           ,("markdown_mmd",  TextReader readMarkdown)
-           ,("commonmark"   , TextReader readCommonMark)
-           ,("rst"          , TextReader readRST)
-           ,("mediawiki"    , TextReader readMediaWiki)
-           ,("vimwiki"      , TextReader readVimwiki)
-           ,("docbook"      , TextReader readDocBook)
-           ,("opml"         , TextReader readOPML)
-           ,("org"          , TextReader readOrg)
-           ,("textile"      , TextReader readTextile) -- TODO : textile+lhs
-           ,("html"         , TextReader readHtml)
-           ,("latex"        , TextReader readLaTeX)
-           ,("haddock"      , TextReader readHaddock)
-           ,("twiki"        , TextReader readTWiki)
+           ,("markdown"     , StringReader readMarkdown)
+           ,("markdown_strict" , StringReader readMarkdown)
+           ,("markdown_phpextra" , StringReader readMarkdown)
+           ,("markdown_github" , StringReader readMarkdown)
+           ,("markdown_mmd",  StringReader readMarkdown)
+           ,("commonmark"   , StringReader readCommonMark)
+           ,("rst"          , StringReader readRST)
+           ,("mediawiki"    , StringReader readMediaWiki)
+           ,("vimwiki"      , StringReader readVimwiki)
+           ,("docbook"      , StringReader readDocBook)
+           ,("opml"         , StringReader readOPML)
+           ,("org"          , StringReader readOrg)
+           ,("textile"      , StringReader readTextile) -- TODO : textile+lhs
+           ,("html"         , StringReader readHtml)
+           ,("latex"        , StringReader readLaTeX)
+           ,("haddock"      , StringReader readHaddock)
+           ,("twiki"        , StringReader readTWiki)
            ,("docx"         , ByteStringReader readDocx)
            ,("odt"          , ByteStringReader readOdt)
-           ,("t2t"          , TextReader readTxt2Tags)
+           ,("t2t"          , StringReader readTxt2Tags)
            ,("epub"         , ByteStringReader readEPUB)
            ]
 
@@ -131,7 +137,7 @@ getReader s =
        Right (readerName, setExts) ->
            case lookup readerName readers of
                    Nothing  -> Left $ "Unknown reader: " ++ readerName
-                   Just  (TextReader r)  -> Right $ TextReader $ \o ->
+                   Just  (StringReader r)  -> Right $ StringReader $ \o ->
                                   r o{ readerExtensions = setExts $
                                             getDefaultExtensions readerName }
                    Just (ByteStringReader r) -> Right $ ByteStringReader $ \o ->
@@ -139,6 +145,5 @@ getReader s =
                                             getDefaultExtensions readerName }
 
 -- | Read pandoc document from JSON format.
-readJSON :: ReaderOptions -> Text -> Either PandocError Pandoc
-readJSON _ =
-mapLeft PandocParseError . eitherDecode' . BL.fromStrict . UTF8.fromText
+readJSON :: ReaderOptions -> String -> Either PandocError Pandoc
+readJSON _ = mapLeft PandocParseError . eitherDecode' . UTF8.fromStringLazy
