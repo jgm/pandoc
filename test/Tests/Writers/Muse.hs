fa515e46f36fa3e73b26b89b721a2de1738cf4e3
Alexander Krotov <ilabdsf@gmail.com>
2017-06-26 16:07:45 +0300

    Muse writer: fix hlint errors (#3764)

--- test/Tests/Writers/Muse.hs
+++ test/Tests/Writers/Muse.hs
@@ -31,14 +31,14 @@ tests = [ testGroup "block elements"
                                            , "Second paragraph."
                                            ]
             ]
-          , "line block" =: lineBlock ([text "Foo", text "bar", text "baz"])
+          , "line block" =: lineBlock [text "Foo", text "bar", text "baz"]
                          =?> unlines [ "<verse>"
                                      , "Foo"
                                      , "bar"
                                      , "baz"
                                      , "</verse>"
                                      ]
-          , "code block" =: codeBlock ("int main(void) {\n\treturn 0;\n}")
+          , "code block" =: codeBlock "int main(void) {\n\treturn 0;\n}"
                          =?> unlines [ "<example>"
                                      , "int main(void) {"
                                      , "\treturn 0;"
