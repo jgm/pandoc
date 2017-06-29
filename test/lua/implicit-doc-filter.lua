0f658eb46ce3630886b733e8c8ed1086af9b3510
John MacFarlane <jgm@berkeley.edu>
2017-06-29 17:08:59 +0200

    data/pandoc.lua:  regularize constructors.
    We now use Pandoc instead of Doc (though Doc remains a deprecated
    Synonym), and we deprecate DoubleQuoted, SingleQuoted,
    InlineMath, and DisplayMath.

--- test/lua/implicit-doc-filter.lua
+++ test/lua/implicit-doc-filter.lua
@@ -2,5 +2,5 @@ function Doc (doc)
   local meta = {}
   local hello = { pandoc.Str "Hello,", pandoc.Space(), pandoc.Str "World!" }
   local blocks = { pandoc.Para(hello) }
-  return pandoc.Doc(blocks, meta)
+  return pandoc.Pandoc(blocks, meta)
 end
