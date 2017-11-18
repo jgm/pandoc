17f6621b21fa9c5b64f58e4da38a3ce53b50f7b9
John MacFarlane <jgm@berkeley.edu>
2017-11-18 13:33:37 -0800

    Update man page lua filter to use text module.

--- doc/lua-filters.md
+++ doc/lua-filters.md
@@ -344,11 +344,14 @@ inside headers), removes footnotes, and replaces links
 with regular text.
 
 ``` lua
+-- we use preloaded text to get a UTF-8 aware 'upper' function
+local text = require('text')
+
 function Header(el)
     if el.level == 1 then
       return pandoc.walk_block(el, {
         Str = function(el)
-            return pandoc.Str(el.text:upper())
+            return pandoc.Str(text.upper(el.text))
         end })
     end
 end
--- man/manfilter.lua
+++ man/manfilter.lua
@@ -1,19 +1,22 @@
--- filters to create the pandoc man page from MANUAL.txt
+-- we use preloaded text to get a UTF-8 aware 'upper' function
+local text = require('text')
 
--- capitalize headers
+-- capitalize level 1 headers
 function Header(el)
     if el.level == 1 then
       return pandoc.walk_block(el, {
         Str = function(el)
-            return pandoc.Str(el.text:upper())
+            return pandoc.Str(text.upper(el.text))
         end })
     end
 end
 
+-- replace links with link text
 function Link(el)
     return el.content
 end
 
+-- remove notes
 function Note(el)
     return {}
 end
