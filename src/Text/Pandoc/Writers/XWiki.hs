83983e8d6bc69582ef7b14041da83407faacaf99
Derek Chen-Becker <github@chen-becker.org>
2018-02-03 10:01:30 -0700

    Fix stale comment for XWiki (copied from MediaWiki)

--- src/Text/Pandoc/Writers/XWiki.hs
+++ src/Text/Pandoc/Writers/XWiki.hs
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
    Stability   : alpha
    Portability : portable
 
-Conversion of 'Pandoc' documents to DokuWiki markup.
+Conversion of 'Pandoc' documents to XWiki markup.
 
 XWiki:  <http://www.xwiki.org/>
 XWiki Syntax:  <http://www.xwiki.org/xwiki/bin/view/Documentation/UserGuide/Features/XWikiSyntax/>
