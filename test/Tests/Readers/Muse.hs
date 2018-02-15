jgm/pandoc

commit 4bfab8f04c105f111d8d4e1c3ed7f7b5c75dbd19

Muse reader: parse directives without parseFromString

src/Text/Pandoc/Readers/Muse.hs
@@ -157,16 +157,14 @@ parseEmacsDirective = do
 parseAmuseDirective :: PandocMonad m => MuseParser m (String, F Inlines)
 parseAmuseDirective = do
   key <- parseDirectiveKey
-  space
-  spaces
-  first <- manyTill anyChar eol
-  rest <- manyTill anyLine endOfDirective
+  many1 spaceChar
+  value <- trimInlinesF . mconcat <$> many1Till inline endOfDirective
   many blankline
-  value <- parseFromString (trimInlinesF . mconcat <$> many inline) $ unlines (first : rest)
   return (key, value)
   where
-    endOfDirective = lookAhead $ endOfInput <|> try (void blankline) <|> try (void parseDirectiveKey)
-    endOfInput     = try $ skipMany blankline >> skipSpaces >> eof
+    endOfDirective = lookAhead $ try (eof <|>
+                                      void (newline >> blankline) <|>
+                                      void (newline >> parseDirectiveKey))
 
 directive :: PandocMonad m => MuseParser m ()
 directive = do

commit 82a0ceaf18e589e8916fbd70e0b13e5945bcc99a

Muse reader: fix directive parsing
This fixes bugs introduced in commit 4bfab8f04c105f111d8d4e1c3ed7f7b5c75dbd19.

src/Text/Pandoc/Readers/Muse.hs
@@ -237,9 +237,7 @@ parseAmuseDirective = do
   many blankline
   return (key, value)
   where
-    endOfDirective = lookAhead $ try (eof <|>
-                                      void (newline >> blankline) <|>
-                                      void (newline >> parseDirectiveKey))
+    endOfDirective = lookAhead $ eof <|> try (newline >> (void blankline <|> void parseDirectiveKey))
 
 directive :: PandocMonad m => MuseParser m ()
 directive = do

test/Tests/Readers/Muse.hs
@@ -537,6 +537,21 @@ tests =
         , test emacsMuse "Disable tables"
           ("#disable-tables t" =?>
           Pandoc (setMeta "disable-tables" (MetaInlines $ toList "t") nullMeta) mempty)
+        , "Multiple directives" =:
+          T.unlines [ "#title Document title"
+                    , "#subtitle Document subtitle"
+                    ] =?>
+          Pandoc (setMeta "title" (MetaInlines $ toList "Document title") $
+                  setMeta "subtitle" (MetaInlines $ toList "Document subtitle") nullMeta) mempty
+        , "Multiline directive" =:
+          T.unlines [ "#title Document title"
+                    , "#notes First line"
+                    , "and second line"
+                    , "#author Name"
+                    ] =?>
+          Pandoc (setMeta "title" (MetaInlines $ toList "Document title") $
+                  setMeta "notes" (MetaInlines $ toList "First line\nand second line") $
+                  setMeta "author" (MetaInlines $ toList "Name") nullMeta) mempty
         ]
       , testGroup "Anchors"
         [ "Anchor" =: