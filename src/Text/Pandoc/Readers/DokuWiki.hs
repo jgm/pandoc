0056cf7a150d45c5d1b45d4ea2e042980738d16c
Alexander Krotov <ilabdsf@gmail.com>
2019-01-05 13:31:46 +0300

    DokuWiki reader: update dates

--- src/Text/Pandoc/Readers/DokuWiki.hs
+++ src/Text/Pandoc/Readers/DokuWiki.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 {-
-  Copyright (C) 2018 Alexander Krotov <ilabdsf@gmail.com>
+  Copyright (C) 2018-2019 Alexander Krotov <ilabdsf@gmail.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
