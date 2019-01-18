69b38d9a79fe836a0bbe78791bd74eab24f65016
Agustín Martín Barbero <ambarbero@ree.es>
2019-01-18 14:08:11 +0100

    docx-writer: Fix incorrectly equating cd:subject and subtitle

--- src/Text/Pandoc/Writers/Docx.hs
+++ src/Text/Pandoc/Writers/Docx.hs
@@ -496,8 +496,9 @@ writeDocx opts doc@(Pandoc meta _) = do
                        Just (MetaList xs) -> map stringify xs
                        _                  -> []
 
+  -- docProps/core.xml
   let docPropsPath = "docProps/core.xml"
-  let extraCoreProps = ["subtitle","abstract","lang","category"]
+  let extraCoreProps = ["subject","abstract","lang","category"]
   let extraCorePropsMap = M.fromList $ zip extraCoreProps
                        ["dc:subject","dc:description","dc:language","cp:category"]
   let docProps = mknode "cp:coreProperties"
@@ -509,13 +510,14 @@ writeDocx opts doc@(Pandoc meta _) = do
           $ mknode "dc:title" [] (stringify $ docTitle meta)
           : mknode "dc:creator" [] (intercalate "; " (map stringify $ docAuthors meta))
           : [ mknode (M.findWithDefault "" k extraCorePropsMap) [] (lookupMetaString k meta)
-            | k <- M.keys (unMeta meta), k `M.member` extraCorePropsMap]
+            | k <- M.keys (unMeta meta), k `elem` extraCoreProps]
           ++ mknode "cp:keywords" [] (intercalate ", " keywords)
           : (\x -> [ mknode "dcterms:created" [("xsi:type","dcterms:W3CDTF")] x
                    , mknode "dcterms:modified" [("xsi:type","dcterms:W3CDTF")] x
                    ]) (formatTime defaultTimeLocale "%FT%XZ" utctime)
   let docPropsEntry = toEntry docPropsPath epochtime $ renderXml docProps
 
+  -- docProps/custom.xml
   let customProperties :: [(String, String)]
       customProperties = [(k, lookupMetaString k meta) | k <- M.keys (unMeta meta)
                          , k `notElem` (["title", "author", "date", "keywords", "references"]
