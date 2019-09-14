7ecae69e27289923b4c6302c8b7dd35dc5da891f
John MacFarlane <jgm@berkeley.edu>
2019-09-13 22:08:42 -0700

    Revert "FB2 reader test: filter CRs."
    This reverts commit e35147d715a737bb854e0c527243f77d970d1b86.

--- test/Tests/Readers/FB2.hs
+++ test/Tests/Readers/FB2.hs
@@ -20,14 +20,14 @@ import qualified Data.ByteString as BS
 import Text.Pandoc
 import Text.Pandoc.UTF8 (toText, fromTextLazy)
 import Data.Text (Text)
-import qualified Data.Text.Lazy as TL
+import Data.Text.Lazy (fromStrict)
 import System.FilePath (replaceExtension)
 
 fb2ToNative :: Text -> Text
 fb2ToNative = purely (writeNative def{ writerTemplate = Just mempty }) . purely (readFB2 def)
 
 fb2Test :: TestName -> FilePath -> TestTree
-fb2Test name path = goldenVsString name native (fromTextLazy . TL.filter (/= '\r') . TL.fromStrict . fb2ToNative . toText <$> BS.readFile path)
+fb2Test name path = goldenVsString name native (fromTextLazy . fromStrict . fb2ToNative . toText <$> BS.readFile path)
   where native = replaceExtension path ".native"
 
 tests :: [TestTree]
