9c67cdb2aeabae9bc4a74d3bc2181e1b68671e70
Emerson Harkin <emerson.f.harkin@gmail.com>
2020-07-01 12:14:21 -0400

    Revert to en-dash separator for LaTeX SIRange

--- src/Text/Pandoc/Readers/LaTeX.hs
+++ src/Text/Pandoc/Readers/LaTeX.hs
@@ -269,18 +269,12 @@ doSIRange = do
   unit <- grouped (mconcat <$> many1 siUnit) <|> siUnit <|> tok
   let emptyOr160 "" = ""
       emptyOr160 _  = "\160"
-  localTo <- translateTerm Translations.To
-  let separator = (if localTo /= ""
-                     then text $ T.toLower localTo 
-                     else "\8211")
   return . mconcat $ [startvalueprefix,
                       emptyOr160 startvalueprefix,
                       startvalue,
                       emptyOr160 unit,
                       unit,
-                      text " ",
-                      separator,
-                      text " ",
+                      " \8211 ", -- An en-dash
                       stopvalueprefix,
                       emptyOr160 stopvalueprefix,
                       stopvalue,
