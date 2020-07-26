d8e5f5ea800bc2f8bb510b36deec11e8281ab7be
Gabriel Nützi <gnuetzi@gmail.com>
2020-07-26 20:24:52 +0200

    Add path manipulation functions

--- src/Text/Pandoc/Lua/Module/System.hs
+++ src/Text/Pandoc/Lua/Module/System.hs
@@ -13,8 +13,25 @@ module Text.Pandoc.Lua.Module.System
   ) where
 
 import Foreign.Lua (Lua, NumResults)
-import Foreign.Lua.Module.System (arch, env, getwd, os,
-                                  with_env, with_tmpdir, with_wd)
+import Foreign.Lua.Module.System (arch,
+                                  os,
+                                  -- * General functions
+                                  env,
+                                  getwd,
+                                  with_env,
+                                  with_tmpdir,
+                                  with_wd,
+                                  -- * Path manipulation functions
+                                  take_directory,
+                                  take_filename,
+                                  take_extensions,
+                                  split_directories,
+                                  has_extension,
+                                  drop_extensions,
+                                  join_path,
+                                  is_relative,
+                                  is_absolute,
+                                  normalise)
 import Text.Pandoc.Lua.Util (addFunction, addField)
 
 import qualified Foreign.Lua as Lua
@@ -23,6 +40,8 @@ import qualified Foreign.Lua as Lua
 pushModule :: Lua NumResults
 pushModule = do
   Lua.newtable
+
+  -- * General functions
   addField "arch" arch
   addField "os" os
   addFunction "environment" env
@@ -30,4 +49,17 @@ pushModule = do
   addFunction "with_environment" with_env
   addFunction "with_temporary_directory" with_tmpdir
   addFunction "with_working_directory" with_wd
+
+  -- * Path manipulation functions
+  addFunction "take_directory"    take_directory
+  addFunction "take_filename"     take_filename
+  addFunction "take_extensions"   take_extensions
+  addFunction "split_directories" split_directories
+  addFunction "has_extension"     has_extension
+  addFunction "drop_extensions"   drop_extensions
+  addFunction "join_path"         join_path
+  addFunction "is_relative"       is_relative
+  addFunction "is_absolute"       is_absolute
+  addFunction "normalise"         normalise
+
   return 1
