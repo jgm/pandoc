jgm/pandoc

commit a157e1a6e029166a090544d23501db5e68e0bbe9

Support numrange, numlist for siunitx.
See #6658.

src/Text/Pandoc/Readers/LaTeX/SIunitx.hs
@@ -4,6 +4,7 @@ module Text.Pandoc.Readers.LaTeX.SIunitx
   , doSI
   , doSIrange
   , doSInum
+  , doSInumlist
   , doSIang
   )
 where
@@ -18,6 +19,7 @@ import qualified Data.Map as M
 import Data.Char (isDigit)
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.List (intersperse)
 
 dosi :: PandocMonad m => LP m Inlines -> LP m Inlines
 dosi tok = grouped (siUnit tok) <|> siUnit tok
@@ -36,12 +38,24 @@ doSI tok = do
                       unit]
 
 doSInum :: PandocMonad m => LP m Inlines
-doSInum = do
-  skipopts
-  value <- untokenize <$> braced
+doSInum = skipopts *> (tonum . untokenize <$> braced)
+
+tonum :: Text -> Inlines
+tonum value =
   case runParser parseNum () "" value of
-    Left _    -> return $ text value
-    Right num -> return num
+    Left _    -> text value
+    Right num -> num
+
+doSInumlist :: PandocMonad m => LP m Inlines
+doSInumlist = do
+  skipopts
+  xs <- map tonum . T.splitOn ";" . untokenize <$> braced
+  case xs of
+    []  -> return mempty
+    [x] -> return x
+    _   -> return $
+             mconcat (intersperse (str "," <> space) (init xs)) <>
+             text ", & " <> last xs
 
 parseNum :: Parser Text () Inlines
 parseNum = mconcat <$> many parseNumPart
@@ -78,14 +92,16 @@ doSIang = do
     _ -> return mempty
 
 -- converts e.g. \SIrange{100}{200}{\ms} to "100 ms--200 ms"
-doSIrange :: PandocMonad m => LP m Inlines -> LP m Inlines
-doSIrange tok = do
+doSIrange :: PandocMonad m => Bool -> LP m Inlines -> LP m Inlines
+doSIrange includeUnits tok = do
   skipopts
   startvalue <- doSInum
   startvalueprefix <- option "" $ bracketed tok
   stopvalue <- doSInum
   stopvalueprefix <- option "" $ bracketed tok
-  unit <- dosi tok
+  unit <- if includeUnits
+             then dosi tok
+             else return mempty
   return . mconcat $ [startvalueprefix,
                       emptyOr160 startvalueprefix,
                       startvalue,

src/Text/Pandoc/Readers/LaTeX.hs
@@ -879,7 +879,9 @@ inlineCommands = M.union inlineLanguageCommands $ M.fromList
   -- siuntix
   , ("si", skipopts *> dosi tok)
   , ("SI", doSI tok)
-  , ("SIrange", doSIrange tok)
+  , ("SIrange", doSIrange True tok)
+  , ("numrange", doSIrange False tok)
+  , ("numlist", doSInumlist)
   , ("num", doSInum)
   , ("ang", doSIang)
   -- hyphenat

commit 734b4c26a990a334b2bbe352ba34257ebb2a6fcd

LaTeX reader: Fix negative numbers in siunitx commands.
The commit a157e1a broke negative numbers, e.g.
`\SI{-33}{\celcius}` or `\num{-3}`. This fixes the regression.

src/Text/Pandoc/Readers/LaTeX/SIunitx.hs
@@ -58,7 +58,7 @@ doSInumlist = do
              text ", & " <> last xs
 
 parseNum :: Parser Text () Inlines
-parseNum = mconcat <$> many parseNumPart
+parseNum = (mconcat <$> many parseNumPart) <* eof
 
 parseNumPart :: Parser Text () Inlines
 parseNumPart =
@@ -71,7 +71,9 @@ parseNumPart =
   parseSpace
  where
   parseDecimalNum = do
-    basenum <- T.pack <$> many1 (satisfy (\c -> isDigit c || c == '.'))
+    pref <- option mempty $ (mempty <$ char '+') <|> ("-" <$ char '-')
+    basenum <- (pref <>) . T.pack
+                <$> many1 (satisfy (\c -> isDigit c || c == '.'))
     uncertainty <- option mempty $ T.pack <$> parseParens
     if T.null uncertainty
        then return $ str basenum