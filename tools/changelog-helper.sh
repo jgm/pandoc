aae1e617a6cff84d737a7195e4e9bc3cb730b978
John MacFarlane <jgm@berkeley.edu>
2021-02-21 19:02:28 -0800

    Fix changelog-helper.sh

--- tools/changelog-helper.sh
+++ tools/changelog-helper.sh
@@ -2,11 +2,11 @@
 # generate preliminary list of changes since changelog
 # was last modified
 
-lastmod=`git log -n2 --format=oneline changelog | awk '{print $1;}'`
-#git log --format=oneline $starthash..HEAD
-files=`git ls-tree -r master --name-only`
+lastmod=`git log -n1 --format=oneline changelog.md | awk '{print $1;}'`
+files=`git ls-tree -r master --name-only | grep --invert-match '^test\/\|^data\/docx\/\|^data\/odt\/\|^data\/pptx\/\|^citeproc\/\|^data\/translations\/'`
 for x in $files
 do
+    echo $x 1>&2
     commits=`git log -n1 $lastmod..HEAD $x`
     if [ ! -z "$commits" ]
     then
@@ -17,7 +17,14 @@ do
             file=$x
         fi
         echo "  * $file"
-        GIT_PAGER=cat git log --pretty=format:'%n%w(78,4,6)+ %s (%aN)%n%n%w(78,6,6)%b%n' -- "$lastmod..HEAD" "$x"
+        git log --pretty=format:'%n%w(78,4,6)+ %s (%aN)%n%n%w(78,6,6)%b%n' "$lastmod..HEAD" "$x"
     fi
 done
+echo "test/" 1>&2
+git log --pretty=format:'%n%w(78,4,6)+ %s (%aN)%n%n%w(78,6,6)%b%n' "$lastmod..HEAD" test/
+git log --pretty=format:'%n%w(78,4,6)+ %s (%aN)%n%n%w(78,6,6)%b%n' "$lastmod..HEAD" data/docx/
+git log --pretty=format:'%n%w(78,4,6)+ %s (%aN)%n%n%w(78,6,6)%b%n' "$lastmod..HEAD" data/odt/
+git log --pretty=format:'%n%w(78,4,6)+ %s (%aN)%n%n%w(78,6,6)%b%n' "$lastmod..HEAD" data/pptx/
+git log --pretty=format:'%n%w(78,4,6)+ %s (%aN)%n%n%w(78,6,6)%b%n' "$lastmod..HEAD" data/translations/
+git log --pretty=format:'%n%w(78,4,6)+ %s (%aN)%n%n%w(78,6,6)%b%n' "$lastmod..HEAD" citeproc/
 
