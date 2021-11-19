4f2eac88aa1cd26d096a88450ad3ae929980e7a6
John MacFarlane <jgm@berkeley.edu>
2021-11-19 09:05:19 -0800

    MediaWiki writer: fix code for generating spans for header IDs.
    We need to generate a span when the header's ID doesn't match
    the one MediaWiki would generate automatically.  But MediaWiki's
    generation scheme is different from ours (it uses uppercase letters,
    and `_` instead of `-`, for example).
    
    This means that in going from markdown -> mediawiki, we'll now get
    spans before almost every heading, unless explicit identifiers are
    used that correspond to the ones MediaWiki auto-generates.
    This is uglier output but it's necessary for internal links to
    work properly.
    
    See #7697.

--- src/Text/Pandoc/Writers/MediaWiki.hs
+++ src/Text/Pandoc/Writers/MediaWiki.hs
@@ -130,7 +130,7 @@ blockToMediaWiki HorizontalRule = return "\n-----\n"
 
 blockToMediaWiki (Header level (ident,_,_) inlines) = do
   opts <- gets stOptions
-  let autoId = uniqueIdent (writerExtensions opts) inlines mempty
+  let autoId = T.replace " " "_" $ stringify inlines
   contents <- inlineListToMediaWiki inlines
   let eqs = T.replicate level "="
   return $
