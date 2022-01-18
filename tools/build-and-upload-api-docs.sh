d2a956255428a7c5751e84c696bb0c387193ec08
John MacFarlane <jgm@berkeley.edu>
2022-01-17 21:00:15 -0800

    Omit --enable-doc in the cabal haddock invocation.
    tools/build-and-upload-api-docs.sh.  I'm not sure what
    this was supposed to do but it seems undocumented.

--- tools/build-and-upload-api-docs.sh
+++ tools/build-and-upload-api-docs.sh
@@ -4,5 +4,5 @@ set -e
 dir=$(mktemp -d dist-docs.XXXXXX)
 trap 'rm -r "$dir"' EXIT
 
-cabal v2-haddock --builddir="$dir" --haddock-for-hackage --enable-doc
+cabal v2-haddock --builddir="$dir" --haddock-for-hackage
 cabal upload -d --publish $dir/*-docs.tar.gz
