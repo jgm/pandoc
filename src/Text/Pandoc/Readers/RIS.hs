0b2adf9617615c426f5a7ae0635385551a0a0688
John MacFarlane <jgm@berkeley.edu>
2022-05-03 10:44:09 -0700

    RIS reader: get right order of names.
    Closes #8055.

--- src/Text/Pandoc/Readers/RIS.hs
+++ src/Text/Pandoc/Readers/RIS.hs
@@ -142,7 +142,7 @@ risRecordToReference keys = addId $ foldr go defref keys
    addName k v r =
      let new = toName [] . B.toList .  B.text $ v
          f Nothing   = Just (NamesVal new)
-         f (Just (NamesVal ns)) = Just (NamesVal (ns ++ new))
+         f (Just (NamesVal ns)) = Just (NamesVal (new ++ ns))
          f (Just x) = Just x
       in r{ referenceVariables =
               M.alter f k (referenceVariables r) }
