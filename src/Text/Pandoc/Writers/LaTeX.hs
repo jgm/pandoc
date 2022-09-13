7ea1d85422a9eacf229d74fb8dd0ad81e46a5549
dvnrussell <dvnrussell@gmail.com>
2022-09-12 23:32:41 -0400

    Defines a command in the LaTeX template for Level 6 Markdown headers Adds a "\subsubparagraph" command definition to the LaTeX template. This command defaults to the current behavior, which compiles level 6 headers (tagged h6 in HTML) to plain text.

--- src/Text/Pandoc/Writers/LaTeX.hs
+++ src/Text/Pandoc/Writers/LaTeX.hs
@@ -674,6 +674,7 @@ sectionHeader classes ident level lst = do
                           3  -> "subsubsection"
                           4  -> "paragraph"
                           5  -> "subparagraph"
+                          6  -> "subsubparagraph"
                           _  -> ""
   inQuote <- gets stInQuote
   let prefix = if inQuote && level' >= 4
@@ -686,7 +687,7 @@ sectionHeader classes ident level lst = do
   let stuffing = star <> optional <> contents
   stuffing' <- hypertarget True ident $
                   text ('\\':sectionType) <> stuffing <> lab
-  return $ if level' > 5
+  return $ if level' > 6
               then txt
               else prefix $$ stuffing'
                    $$ if unnumbered && not unlisted
