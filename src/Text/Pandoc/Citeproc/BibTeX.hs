jgm/pandoc

commit 6fb2973a582116cd515c6f7e68794cca22955511

Fix parsing of `%` in bibtex fields.
Closes #7678 (a bug introduced by 0a45f26).

src/Text/Pandoc/Citeproc/BibTeX.hs
@@ -844,11 +844,10 @@ inBraces :: BibParser Text
 inBraces = do
   char '{'
   res <- manyTill
-         (  take1WhileP (\c -> c /= '{' && c /= '}' && c /= '\\' && c /= '%')
-        <|> (char '\\' >> (  (char '{' >> return "\\{")
-                         <|> (char '}' >> return "\\}")
-                         <|> return "\\"))
-        <|> ("" <$ (char '%' >> anyLine))
+         (  take1WhileP (\c -> c /= '{' && c /= '}' && c /= '\\')
+        <|> (char '\\' >> (do c <- oneOf "{}"
+                              return $ T.pack ['\\',c])
+                         <|> return "\\")
         <|> (braced <$> inBraces)
          ) (char '}')
   return $ T.concat res

commit c0ff59c272109d863beefcef0f796f758eaa8c84

BibTeX parser: fix handling of `%` in url field.
`%` does not function as a comment character inside `url`
(where URL-encoding is common).

Commit 6fb2973a582116cd515c6f7e68794cca22955511 mistakenly
took this reassignment of `%` to be a general feature of
braced (but not quoted) BibTeX fields.

This commit restores the correct behavior of `%` in braced fields
other than `url`, and corrects the behavior of `%` in `url`
when the value is quoted.

Closes #7678 (again).

src/Text/Pandoc/Citeproc/BibTeX.hs
@@ -846,10 +846,19 @@ inBraces :: BibParser Text
 inBraces = do
   char '{'
   res <- manyTill
+         (  take1WhileP (\c -> c /= '{' && c /= '}' && c /= '\\' && c /= '%')
+        <|> (char '\\' >> T.cons '\\' . T.singleton <$> anyChar)
+        <|> ("" <$ (char '%' >> anyLine))
+        <|> (braced <$> inBraces)
+         ) (char '}')
+  return $ T.concat res
+
+inBracesURL :: BibParser Text
+inBracesURL = do
+  char '{'
+  res <- manyTill
          (  take1WhileP (\c -> c /= '{' && c /= '}' && c /= '\\')
-        <|> (char '\\' >> (do c <- oneOf "{}"
-                              return $ T.pack ['\\',c])
-                         <|> return "\\")
+        <|> (char '\\' >> T.cons '\\' . T.singleton <$> anyChar)
         <|> (braced <$> inBraces)
          ) (char '}')
   return $ T.concat res
@@ -866,6 +875,14 @@ inQuotes = do
                <|> ("" <$ (char '%' >> anyLine))
                <|> braced <$> inBraces
             ) (char '"')
+
+inQuotesURL :: BibParser Text
+inQuotesURL = do
+  char '"'
+  T.concat <$> manyTill
+             ( take1WhileP (\c -> c /= '{' && c /= '"' && c /= '\\')
+               <|> (char '\\' >> T.cons '\\' . T.singleton <$> anyChar)
+             ) (char '"')
 
 fieldName :: BibParser Text
 fieldName = resolveAlias . T.toLower
@@ -902,7 +919,9 @@ entField = do
   spaces'
   char '='
   spaces'
-  vs <- (expandString <|> inQuotes <|> inBraces <|> rawWord) `sepBy`
+  let inQ = if k == "url" then inQuotesURL else inQuotes
+  let inB = if k == "url" then inBracesURL else inBraces
+  vs <- (expandString <|> inQ <|> inB <|> rawWord) `sepBy`
             try (spaces' >> char '#' >> spaces')
   spaces'
   return (k, T.concat vs)