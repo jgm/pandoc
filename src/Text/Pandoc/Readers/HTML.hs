740396d1ed91d03534b99ad8ccf545d916ee3f7a
Justin Wood <jwood@starry.com>
2022-09-28 11:20:53 -0400

    HTML Reader: Tests for 'tt' and 'code' elements (#8330).
    tt is a deprecated element in HTML, but is still used in the wild in
    some places, support reading it as just another 'code' element.
    
    Commit 4abb9d0ad8dbb88fbc443a78d5a1b116cb7a5816 was originally
    part of this PR as well.

--- src/Text/Pandoc/Readers/HTML.hs
+++ src/Text/Pandoc/Readers/HTML.hs
@@ -662,7 +662,7 @@ inline = pTagText <|> do
         "img" -> pImage
         "svg" -> pSvg
         "bdo" -> pBdo
-        "tt"  -> pCode
+        "tt" -> pCode
         "code" -> pCode
         "samp" -> pCodeWithClass "samp" "sample"
         "var" -> pCodeWithClass "var" "variable"
