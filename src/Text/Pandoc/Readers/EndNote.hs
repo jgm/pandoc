314c2d1d322249e76bf1729ac1a3c4a16e84b603
John MacFarlane <jgm@berkeley.edu>
2022-11-10 10:14:59 -0800

    Better error when parsing EndNote references fails.
    (Tell them that it concerns EndNote references.)

--- src/Text/Pandoc/Readers/EndNote.hs
+++ src/Text/Pandoc/Readers/EndNote.hs
@@ -74,10 +74,10 @@ readEndNoteXML _opts inp = do
 readEndNoteXMLCitation :: PandocMonad m
                     => Sources -> m (Citeproc.Citation Text)
 readEndNoteXMLCitation sources = do
-  tree <- either (throwError . PandocXMLError "") return $
+  tree <- either (throwError . PandocXMLError "EndNote references") return $
               parseXMLElement (TL.fromStrict . sourcesToText $ sources)
   unless (qName (elName tree) == "EndNote") $
-    throwError $ PandocXMLError "" "Expected EndNote element"
+    throwError $ PandocXMLError "EndNote references" "Expected EndNote element"
   let items = map toCitationItem $ filterElementsName (name "Cite") tree
   return $ Citeproc.Citation{
                      Citeproc.citationId = Nothing
@@ -88,7 +88,7 @@ readEndNoteXMLCitation sources = do
 readEndNoteXMLReferences :: PandocMonad m
                          => Sources -> m [Reference Text]
 readEndNoteXMLReferences sources = do
-  tree <- either (throwError . PandocXMLError "") return $
+  tree <- either (throwError . PandocXMLError "EndNote references") return $
               parseXMLElement (TL.fromStrict . sourcesToText $ sources)
   let records = filterElementsName (name "record") tree
   return $ map recordToReference records
