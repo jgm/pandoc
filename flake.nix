b8ce955e675776240718dc68c2eb80485cbe0817
John MacFarlane <jgm@berkeley.edu>
2023-03-08 20:44:07 -0800

    Improve flake.nix.

--- flake.nix
+++ flake.nix
@@ -1,14 +1,9 @@
 {
-  description = "srid/haskell-template: Nix template for Haskell projects";
+  description = "Pandoc development";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    flake-root.url = "github:srid/flake-root";
-    mission-control.url = "github:Platonic-Systems/mission-control";
-
     nixpkgs-140774-workaround.url = "github:srid/nixpkgs-140774-workaround";
   };
 
@@ -17,85 +12,36 @@
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
         inputs.haskell-flake.flakeModule
-        inputs.treefmt-nix.flakeModule
-        inputs.flake-root.flakeModule
-        inputs.mission-control.flakeModule
       ];
       perSystem = { self', system, lib, config, pkgs, ... }: {
-        # The "main" project. You can have multiple projects, but this template
-        # has only one.
         haskellProjects.main = {
           imports = [
             inputs.nixpkgs-140774-workaround.haskellFlakeProjectModules.default
           ];
-          # packages.haskell-template.root = ./.;  # Auto-discovered by haskell-flake
           overrides = self: super: { };
           devShell = {
             tools = hp: {
-              treefmt = config.treefmt.build.wrapper;
-            } // config.treefmt.build.programs;
+              inherit (hp)
+                hlint
+                ormolu
+                weeder
+                stylish-haskell
+                ghcid;
+            };
             hlsCheck.enable = true;
           };
-        };
-
-        # Auto formatters. This also adds a flake check to ensure that the
-        # source tree was auto formatted.
-        treefmt.config = {
-          inherit (config.flake-root) projectRootFile;
-          package = pkgs.treefmt;
-
-          programs.ormolu.enable = true;
-          programs.nixpkgs-fmt.enable = true;
-          programs.cabal-fmt.enable = true;
-          programs.hlint.enable = true;
-
-          # We use fourmolu
-          programs.ormolu.package = pkgs.haskellPackages.fourmolu;
-          settings.formatter.ormolu = {
-            options = [
-              "--ghc-opt"
-              "-XImportQualifiedPost"
-            ];
-          };
-        };
-
-        # Dev shell scripts.
-        mission-control.scripts = {
-          docs = {
-            description = "Start Hoogle server for project dependencies";
-            exec = ''
-              echo http://127.0.0.1:8888
-              hoogle serve -p 8888 --local
-            '';
-            category = "Dev Tools";
-          };
-          repl = {
-            description = "Start the cabal repl";
-            exec = ''
-              cabal repl "$@"
-            '';
-            category = "Dev Tools";
-          };
-          fmt = {
-            description = "Format the source tree";
-            exec = "${lib.getExe config.treefmt.build.wrapper}";
-            category = "Dev Tools";
-          };
-          run = {
-            description = "Run the project with ghcid auto-recompile";
-            exec = ''
-              ghcid -c "cabal repl exe:haskell-template" --warnings -T :main
-            '';
-            category = "Primary";
+          packages = {
+            pandoc.root = ./.;
+            pandoc-cli.root = ./pandoc-cli;
+            pandoc-server.root = ./pandoc-server;
+            pandoc-lua-engine.root = ./pandoc-lua-engine;
           };
         };
 
         # Default package.
-        packages.default = self'.packages.main-haskell-template;
+        packages.default = self'.packages.pandoc;
 
-        # Default shell.
-        devShells.default =
-          config.mission-control.installToDevShell self'.devShells.main;
+        devShells.default = self'.devShells.main;
       };
     };
 }
