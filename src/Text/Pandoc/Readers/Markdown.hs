5e668118751b50aaf7f6ab675dd5dc628ced30bc
John MacFarlane <jgm@berkeley.edu>
2023-10-20 10:27:49 -0700

    Markdown reader: fix blindspot with superscript in links.
    Previously `[^super^](#ref)` wasn't parsed as a link, due to
    code that was meant to prevent footnote markers from being
    recognized as reference links.  This commit tightens up that
    code to avoid this bad effect.
    
    We have also added a new restriction on footnote labels: they
    cannot contain the characters `^`, `[`, or `]`. Though this is
    technically a breaking change, we suspect that the impact will
    be minimal, as it's very unlikely people would be using these
    characters in their note labels.
    
    Closes #8981.

--- src/Text/Pandoc/Readers/Markdown.hs
+++ src/Text/Pandoc/Readers/Markdown.hs
@@ -142,12 +142,6 @@ isHruleChar _   = False
 setextHChars :: [Char]
 setextHChars = "=-"
 
-isBlank :: Char -> Bool
-isBlank ' '  = True
-isBlank '\t' = True
-isBlank '\n' = True
-isBlank _    = False
-
 --
 -- auxiliary functions
 --
@@ -416,7 +410,9 @@ abbrevKey = do
     return $ return mempty
 
 noteMarker :: PandocMonad m => MarkdownParser m Text
-noteMarker = string "[^" >> many1TillChar (satisfy $ not . isBlank) (char ']')
+noteMarker = string "[^" >>
+  many1TillChar (satisfy (`notElem` ['\r','\n','\t',' ','^','[',']']))
+                (char ']')
 
 rawLine :: PandocMonad m => MarkdownParser m Text
 rawLine = try $ do
@@ -1815,7 +1811,7 @@ endline = try $ do
 -- a reference label for a link
 reference :: PandocMonad m => MarkdownParser m (F Inlines, Text)
 reference = do
-  guardDisabled Ext_footnotes <|> notFollowedBy' (string "[^")
+  guardDisabled Ext_footnotes <|> notFollowedBy' noteMarker
   withRaw $ trimInlinesF <$> inlinesInBalancedBrackets
 
 parenthesizedChars :: PandocMonad m => MarkdownParser m Text
