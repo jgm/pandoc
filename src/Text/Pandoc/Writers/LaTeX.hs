3d4ee36517f944d108e715c843dac2de34cb7299
Hikaru Ibayashi <hikaru.ccm1023@gmail.com>
2023-11-04 19:39:05 -0700

    Bug fix for the the Latex writer issue #8982
    Latex Writer convert
    `emph (str "This sentence" <> note (para (str "paragraph1") <> para (str "paragraph2")) <> str " has footnote.")`
    to
    `"\\emph{This sentence}\\footnote{paragraph1\n\n  paragraph2}\\emph{ has footnote.}"`
    instead of
    `"\\emph{This sentence\\footnote{paragraph1\n\n  paragraph2} has footnote.}"`

--- src/Text/Pandoc/Writers/LaTeX.hs
+++ src/Text/Pandoc/Writers/LaTeX.hs
@@ -799,7 +799,20 @@ inlineToLaTeX (Span (id',classes,kvs) ils) = do
     (if null cmds
         then braces contents
         else foldr inCmd contents cmds)
-inlineToLaTeX (Emph lst) = inCmd "emph" <$> inlineListToLaTeX lst
+inlineToLaTeX (Emph lst)
+    | any isNote lst = case break isNote lst of
+                        (nonNote, []) ->
+                          inCmd "emph" <$> inlineListToLaTeX nonNote
+                        (nonNote, note:rest) -> do
+                          nonNoteDoc <- inCmd "emph" <$> inlineListToLaTeX nonNote
+                          noteDoc <- inlineListToLaTeX [note]
+                          restDoc <- inlineListToLaTeX [Emph rest]
+                          return (nonNoteDoc <> noteDoc <> restDoc)
+    | otherwise      = inCmd "emph" <$> inlineListToLaTeX lst
+    where
+      isNote :: Inline -> Bool
+      isNote (Note _) = True
+      isNote _        = False
 inlineToLaTeX (Underline lst) = do
   modify $ \st -> st{ stStrikeout = True } -- this gives us the soul package
   inCmd "ul" <$> inlineListToLaTeX lst
