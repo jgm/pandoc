jgm/pandoc

commit c9bf4da74ffa21b4cf4b5fdfdcf02807db5dc5bd

Docx writer: ensure that elements in settings are ordered correctly.
The elements must occur in a specific order. This was being
messed up when integrating a custom reference.docx. Closes #9264.

src/Text/Pandoc/Writers/Docx.hs
@@ -28,7 +28,7 @@ import Codec.Archive.Zip
       toEntry,
       Entry(eRelativePath) )
 import Control.Applicative ((<|>))
-import Control.Monad (MonadPlus(mplus), unless, when)
+import Control.Monad (MonadPlus(mplus), unless, when, foldM)
 import Control.Monad.Except (catchError, throwError)
 import Control.Monad.Reader
     ( asks, MonadReader(local), MonadTrans(lift), ReaderT(runReaderT) )
@@ -508,30 +508,105 @@ writeDocx opts doc = do
   -- adds references to footnotes or endnotes we don't have...
   -- we do, however, copy some settings over from reference
   let settingsPath = "word/settings.xml"
-      settingsList = [ "zoom"
-                     , "mirrorMargins"
+
+  settingsEntry <- copyChildren refArchive distArchive settingsPath epochtime
+                      -- note: these must go in the following order:
+                     [ "writeProtection"
+                     , "view"
+                     , "zoom"
+                     , "removePersonalInformation"
+                     , "removeDateAndTime"
+                     , "doNotDisplayPageBoundaries"
+                     , "displayBackgroundShape"
+                     , "printPostScriptOverText"
+                     , "printFractionalCharacterWidth"
+                     , "printFormsData"
+                     , "embedTrueTypeFonts"
                      , "embedSystemFonts"
+                     , "saveSubsetFonts"
+                     , "saveFormsData"
+                     , "mirrorMargins"
+                     , "alignBordersAndEdges"
+                     , "bordersDoNotSurroundHeader"
+                     , "bordersDoNotSurroundFooter"
+                     , "gutterAtTop"
+                     , "hideSpellingErrors"
+                     , "hideGrammaticalErrors"
+                     , "activeWritingStyle"
+                     , "proofState"
+                     , "formsDesign"
+                     , "attachedTemplate"
+                     , "linkStyles"
+                     , "stylePaneFormatFilter"
+                     , "stylePaneSortMethod"
+                     , "documentType"
+                     , "mailMerge"
+                     , "revisionView"
+                     , "trackRevisions"
                      , "doNotTrackMoves"
+                     , "doNotTrackFormatting"
+                     , "documentProtection"
+                     , "autoFormatOverride"
+                     , "styleLockTheme"
+                     , "styleLockQFSet"
                      , "defaultTabStop"
+                     , "autoHyphenation"
+                     , "consecutiveHyphenLimit"
+                     , "hyphenationZone"
+                     , "doNotHyphenateCaps"
+                     , "showEnvelope"
+                     , "summaryLength"
+                     , "clickAndTypeStyle"
+                     , "defaultTableStyle"
+                     , "evenAndOddHeaders"
+                     , "bookFoldRevPrinting"
+                     , "bookFoldPrinting"
+                     , "bookFoldPrintingSheets"
                      , "drawingGridHorizontalSpacing"
                      , "drawingGridVerticalSpacing"
                      , "displayHorizontalDrawingGridEvery"
                      , "displayVerticalDrawingGridEvery"
+                     , "doNotUseMarginsForDrawingGridOrigin"
+                     , "drawingGridHorizontalOrigin"
+                     , "drawingGridVerticalOrigin"
+                     , "doNotShadeFormData"
+                     , "noPunctuationKerning"
                      , "characterSpacingControl"
+                     , "printTwoOnOne"
+                     , "strictFirstAndLastChars"
+                     , "noLineBreaksAfter"
+                     , "noLineBreaksBefore"
                      , "savePreviewPicture"
-                     , "mathPr"
-                     , "themeFontLang"
-                     , "decimalSymbol"
-                     , "listSeparator"
-                     , "autoHyphenation"
-                     , "consecutiveHyphenLimit"
-                     , "hyphenationZone"
-                     , "doNotHyphenateCap"
-                     , "evenAndOddHeaders"
-                     , "proofState"
+                     , "doNotValidateAgainstSchema"
+                     , "saveInvalidXml"
+                     , "ignoreMixedContent"
+                     , "alwaysShowPlaceholderText"
+                     , "doNotDemarcateInvalidXml"
+                     , "saveXmlDataOnly"
+                     , "useXSLTWhenSaving"
+                     , "saveThroughXslt"
+                     , "showXMLTags"
+                     , "alwaysMergeEmptyNamespace"
+                     , "updateFields"
+                     , "hdrShapeDefaults"
+                     , "footnotePr"
+                     , "endnotePr"
                      , "compat"
-                     ]
-  settingsEntry <- copyChildren refArchive distArchive settingsPath epochtime settingsList
+                     , "docVars"
+                     , "rsids"
+                     , "attachedSchema"
+                     , "themeFontLang"
+                     , "clrSchemeMapping"
+                     , "doNotIncludeSubdocsInStats"
+                     , "doNotAutoCompressPictures"
+                     , "forceUpgrade"
+                     , "captions"
+                     , "readModeInkLockDown"
+                     , "smartTagType"
+                     , "shapeDefaults"
+                     , "doNotEmbedSmartTags"
+                     , "decimalSymbol"
+                     , "listSeparator" ]
 
   let entryFromArchive arch path =
          maybe (throwError $ PandocSomeError
@@ -639,17 +714,17 @@ copyChildren :: (PandocMonad m)
 copyChildren refArchive distArchive path timestamp elNames = do
   ref  <- parseXml refArchive distArchive path
   dist <- parseXml distArchive distArchive path
-  let elsToCopy =
-        map cleanElem $ filterChildrenName (\e -> qName e `elem` elNames) ref
-  let elsToKeep =
-        [e | Elem e <- elContent dist, not (any (hasSameNameAs e) elsToCopy)]
-  return $ toEntry path timestamp $ renderXml dist{
-      elContent = map Elem elsToKeep ++ map Elem elsToCopy
-    }
+  els <- foldM (addEl ref dist) [] (reverse elNames)
+  return $ toEntry path timestamp
+         $ renderXml dist{ elContent = map cleanElem els }
   where
-    hasSameNameAs (Element {elName = n1}) (Element {elName = n2}) =
-      qName n1 == qName n2
-    cleanElem el@Element{elName=name} = el{elName=name{qURI=Nothing}}
+    addEl ref dist els name =
+      case filterChildName (hasName name) ref `mplus`
+             filterChildName (hasName name) dist of
+        Just el -> pure (el : els)
+        Nothing -> pure els
+    hasName name = (== name) . qName
+    cleanElem el@Element{elName=name} = Elem el{elName=name{qURI=Nothing}}
 
 -- this is the lowest number used for a list numId
 baseListId :: Int

commit 223cfd1d84ae0fc08c56803bc34c86e3e71a5775

fix(docx): fix validation error on endnotePr
Copying `endnotePr` causes validation errors, because it is now
referencing something that doesn't exist in the document:
```
  {
        "FilePath": "test/docx/golden/custom_style_reference.docx",
        "ValidationErrors": "[{\"Description\":\"Element 'w:endnote' referenced by 'endnote@http://schemas.openxmlformats.org/wordprocessingml/2006/main:id' does not exist in part '/MainDocumentPart/EndnotesPart'. The reference value is '0'.\",\"Path\":{\"NamespacesDefinitions\":[\"xmlns:w=\\\"http://schemas.openxmlformats.org/wordprocessingml/2006/main\\\"\"],\"Namespaces\":{},\"XPath\":\"/w:settings[1]/w:endnotePr[1]/w:endnote[2]\",\"PartUri\":\"/word/settings.xml\"},\"Id\":\"Sem_MissingReferenceElement\",\"ErrorType\":\"Semantic\"},{\"Description\":\"Element 'w:endnote' referenced by 'endnote@http://schemas.openxmlformats.org/wordprocessingml/2006/main:id' does not exist in part '/MainDocumentPart/EndnotesPart'. The reference value is '-1'.\",\"Path\":{\"NamespacesDefinitions\":[\"xmlns:w=\\\"http://schemas.openxmlformats.org/wordprocessingml/2006/main\\\"\"],\"Namespaces\":{},\"XPath\":\"/w:settings[1]/w:endnotePr[1]/w:endnote[1]\",\"PartUri\":\"/word/settings.xml\"},\"Id\":\"Sem_MissingReferenceElement\",\"ErrorType\":\"Semantic\"}]"
  }
```

For now don't copy this element, it wasn't copied before, and it doesn't
seem necessary to fix the ordering problems we had with settings.

Fixes: c9bf4da74 ("Docx writer: ensure that elements in settings are ordered correctly.")

Signed-off-by: Edwin Török <edwin@etorok.net>

src/Text/Pandoc/Writers/Docx.hs
@@ -590,7 +590,6 @@ writeDocx opts doc = do
                      , "updateFields"
                      , "hdrShapeDefaults"
                      , "footnotePr"
-                     , "endnotePr"
                      , "compat"
                      , "docVars"
                      , "rsids"