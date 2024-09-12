9e44bb6d40d1cf3ad25d2d4d7d460a33365783b4
Evan Silberman <evan@jklol.net>
2024-09-12 00:40:51 -0700

    tktk: Unify roff escapes

--- src/Text/Pandoc/Readers/Mdoc/Lex.hs
+++ src/Text/Pandoc/Readers/Mdoc/Lex.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
 {- |
    Module      : Text.Pandoc.Readers.Mdoc.Lex
    Copyright   : Copyright (C) 2018-2020 Yan Pashkovsky and John MacFarlane
@@ -21,20 +24,16 @@ module Text.Pandoc.Readers.Mdoc.Lex
   )
 where
 
-import Control.Monad (void, mzero, mplus, guard)
+import Control.Monad (void, guard)
 import Control.Monad.Except (throwError)
-import Text.Pandoc.Class.PandocMonad (PandocMonad(..), report)
-import Data.Char (chr, isAscii, isAlphaNum)
+import Text.Pandoc.Class.PandocMonad (PandocMonad(..))
+import Data.Char (isAlphaNum)
 import Data.Default (Default)
-import qualified Data.Map as M
 import qualified Data.Text as T
-import Text.Pandoc.Logging (LogMessage(..))
 import Text.Pandoc.Options
 import Text.Pandoc.Parsing
-import Text.Pandoc.Shared (safeRead)
-import Text.Pandoc.RoffChar (characterCodes, combiningAccents)
+import Text.Pandoc.Readers.Roff.Escape
 import qualified Data.Sequence as Seq
-import qualified Data.Text.Normalize as Normalize
 
 -- import Debug.Trace (traceShowId)
 
@@ -80,14 +79,20 @@ newtype MdocTokens = MdocTokens { unRoffTokens :: Seq.Seq MdocToken }
 singleTok :: MdocToken -> MdocTokens
 singleTok t = MdocTokens (Seq.singleton t)
 
-data RoffState = RoffState { tableTabChar :: Char
-                           } deriving Show
+data RoffState = RoffState  deriving Show
 
 instance Default RoffState where
-  def = RoffState { tableTabChar = '\t'
-                  }
+  def = RoffState
 
-type RoffLexer m = ParsecT Sources RoffState m
+type Lexer m = ParsecT Sources RoffState m
+
+instance RoffMonad MdocTokens where
+  type Token MdocTokens = T.Text
+  type State MdocTokens = RoffState
+  expandString = return ()
+  escString = return mempty
+  emit = id
+  backslash = (mempty <* char '\\') <|> (mempty <* string "\\E")
 
 --
 -- Lexer: T.Text -> RoffToken
@@ -98,164 +103,10 @@ eofline = do
   void newline <|> eof
   return Eol
 
-characterCodeMap :: M.Map T.Text Char
-characterCodeMap =
-  M.fromList $ map (\(x,y) -> (y,x)) characterCodes
-
-combiningAccentsMap :: M.Map T.Text Char
-combiningAccentsMap =
-  M.fromList $ map (\(x,y) -> (y,x)) combiningAccents
-
-escape :: PandocMonad m => RoffLexer m T.Text
-escape = try $ do
-  backslash
-  escapeGlyph <|> escapeNormal
-
-escapeGlyph :: PandocMonad m => RoffLexer m T.Text
-escapeGlyph = do
-  c <- lookAhead (oneOf ['[','('])
-  escapeArg >>= resolveGlyph c
-
-resolveGlyph :: PandocMonad m => Char -> T.Text -> RoffLexer m T.Text
-resolveGlyph delimChar glyph = do
-  let cs = T.replace "_u" " u" glyph -- unicode glyphs separated by _
-  (case T.words cs of
-      []  -> mzero
-      [s] -> case M.lookup s characterCodeMap `mplus` readUnicodeChar s of
-               Nothing -> mzero
-               Just c  -> return $ T.singleton c
-      (s:ss) -> do
-        basechar <- case M.lookup s characterCodeMap `mplus`
-                         readUnicodeChar s of
-                      Nothing ->
-                        case T.unpack s of
-                          [ch] | isAscii ch && isAlphaNum ch ->
-                                 return ch
-                          _ -> mzero
-                      Just c  -> return c
-        let addAccents [] xs = return $ Normalize.normalize Normalize.NFC $
-                                        T.reverse xs
-            addAccents (a:as) xs =
-              case M.lookup a combiningAccentsMap `mplus` readUnicodeChar a of
-                Just x  -> addAccents as $ T.cons x xs
-                Nothing -> mzero
-        addAccents ss (T.singleton basechar) >>= \xs -> return xs)
-      <|> case delimChar of
-            '['  -> escUnknown ("\\[" <> glyph <> "]")
-            '('  -> escUnknown ("\\(" <> glyph)
-            '\'' -> escUnknown ("\\C'" <> glyph <> "'")
-            _    -> Prelude.fail "resolveGlyph: unknown glyph delimiter"
-
-readUnicodeChar :: T.Text -> Maybe Char
-readUnicodeChar t = case T.uncons t of
-  Just ('u', cs) | T.length cs > 3 -> chr <$> safeRead ("0x" <> cs)
-  _ -> Nothing
-
-escapeNormal :: PandocMonad m => RoffLexer m T.Text
-escapeNormal = do
-  c <- noneOf "{}"
-  let groffSkip = [escapeArg, countChar 1 (satisfy (/='\n'))]
-  case c of
-    ' ' -> return " " -- mandoc_char(7) says this should be a nonbreaking space
-    '"' -> mempty <$ skipMany (satisfy (/='\n')) -- line comment
-    '#' -> mempty <$ manyTill anyChar newline
-    '%' -> return mempty  -- optional hyphenation
-    '&' -> return mempty  -- nonprintable zero-width
-    ')' -> return mempty  -- nonprintable zero-width
-    '*' -> escIgnore '*' groffSkip
-    ',' -> return mempty  -- to fix spacing after roman
-    '-' -> return "-"
-    '.' -> return "."
-    '/' -> return mempty  -- to fix spacing before roman
-    '0' -> return "\x2007" -- digit-width space
-    ':' -> return mempty  -- zero-width break
-    'A' -> escIgnore 'A' [quoteArg]
-    'B' -> escIgnore 'B' [quoteArg]
-    'C' -> quoteArg >>= resolveGlyph '\''
-    'D' -> escIgnore 'D' [quoteArg]
-    'F' -> escIgnore 'F' groffSkip
-    'H' -> escIgnore 'H' [quoteArg]
-    'L' -> escIgnore 'L' [quoteArg]
-    'M' -> escIgnore 'M' groffSkip
-    'N' -> escIgnore 'N' [quoteArg]
-    'O' -> escIgnore 'O' groffSkip
-    'R' -> escIgnore 'R' [quoteArg]
-    'S' -> escIgnore 'S' [quoteArg]
-    'V' -> escIgnore 'V' groffSkip
-    'X' -> escIgnore 'X' [quoteArg]
-    'Y' -> escIgnore 'Y' groffSkip
-    'Z' -> escIgnore 'Z' [quoteArg]
-    '\'' -> return "'"
-    '\n' -> return mempty  -- line continuation
-    '^' -> return "\x200A" -- 1/12 em space
-    '_' -> return "_"
-    '`' -> return "`"
-    'a' -> return mempty  -- "non-interpreted leader character"
-    'b' -> escIgnore 'b' [quoteArg]
-    'c' -> return mempty  -- interrupt text processing
-    'd' -> escIgnore 'd' [] -- forward down 1/2em
-    'e' -> return "\\"
-    'f' -> escIgnore 'f' groffSkip
-    'g' -> escIgnore 'g' groffSkip
-    'h' -> escIgnore 'h' [quoteArg]
-    'k' -> escIgnore 'k' groffSkip
-    'l' -> escIgnore 'l' [quoteArg]
-    'm' -> escIgnore 'm' groffSkip
-    'n' -> escIgnore 'm' groffSkip
-    'o' -> escIgnore 'o' [quoteArg]
-    'p' -> escIgnore 'p' []
-    'r' -> escIgnore 'r' []
-    's' -> escIgnore 's' [escapeArg, signedNumber]
-    't' -> return "\t"
-    'u' -> escIgnore 'u' []
-    'v' -> escIgnore 'v' [quoteArg]
-    'w' -> escIgnore 'w' [quoteArg]
-    'x' -> escIgnore 'x' [quoteArg]
-    'z' -> escIgnore 'z' [countChar 1 anyChar]
-    '|' -> return "\x2006" --1/6 em space
-    '~' -> return "\160" -- nonbreaking space
-    '\\' -> return "\\"
-    _   -> return $ T.singleton c
-    -- man 7 groff: "If  a  backslash  is followed by a character that
-    -- does not constitute a defined escape sequence, the backslash
-    -- is  silently  ignored  and  the character maps to itself."
-
-escIgnore :: PandocMonad m
-          => Char
-          -> [RoffLexer m T.Text]
-          -> RoffLexer m T.Text
-escIgnore c argparsers = do
-  pos <- getPosition
-  arg <- snd <$> withRaw (choice argparsers) <|> return ""
-  report $ SkippedContent ("\\" <> T.cons c arg) pos
-  return mempty
-
-escUnknown :: PandocMonad m => T.Text -> RoffLexer m T.Text
-escUnknown s = do
-  pos <- getPosition
-  report $ SkippedContent s pos
-  return "\xFFFD"
-
-signedNumber :: PandocMonad m => RoffLexer m T.Text
-signedNumber = try $ do
-  sign <- option "" ("-" <$ char '-' <|> "" <$ char '+')
-  ds <- many1Char digit
-  return (sign <> ds)
-
--- Parses: [..] or (..
-escapeArg :: PandocMonad m => RoffLexer m T.Text
-escapeArg = choice
-    [ char '[' *> manyTillChar (noneOf ['\n',']']) (char ']')
-    , char '(' *> countChar 2 (satisfy (/='\n'))
-    ]
-
--- Parses: '..'
-quoteArg :: PandocMonad m => RoffLexer m T.Text
-quoteArg = char '\'' *> manyTillChar (noneOf ['\n','\'']) (char '\'')
 
 -- separate function from lexMacro since real man files sometimes do not
 -- follow the rules
-lexComment :: PandocMonad m => RoffLexer m MdocTokens
+lexComment :: PandocMonad m => Lexer m MdocTokens
 lexComment = do
   try $ string ".\\\""
   skipMany $ noneOf "\n"
@@ -263,13 +114,13 @@ lexComment = do
   return mempty
 
 
-argText :: PandocMonad m => RoffLexer m T.Text
+argText :: PandocMonad m => Lexer m T.Text
 argText = mconcat <$> many1 (escape <|> regularText)
 
-spaceTabChar :: PandocMonad m => RoffLexer m T.Text
+spaceTabChar :: PandocMonad m => Lexer m T.Text
 spaceTabChar = T.singleton <$> spaceChar
 
-quotedArg :: PandocMonad m => RoffLexer m T.Text
+quotedArg :: PandocMonad m => Lexer m T.Text
 quotedArg = do
   quoteChar
   t <- mconcat <$> many (escape <|> regularText <|> innerQuote <|> spaceTabChar)
@@ -281,23 +132,19 @@ quotedArg = do
       string "\"\""
       return "\""
 
-anyText :: PandocMonad m => RoffLexer m T.Text
+anyText :: PandocMonad m => Lexer m T.Text
 anyText = escape <|> regularText <|> quoteChar <|> spaceTabChar
 
-backslash :: PandocMonad m => RoffLexer m ()
-backslash = 
-  (mempty <* char '\\') <|> (mempty <* string "\\E")
-
-regularText :: PandocMonad m => RoffLexer m T.Text
+regularText :: PandocMonad m => Lexer m T.Text
 regularText = many1Char $ noneOf "\n\r\t \\\""
 
-quoteChar :: PandocMonad m => RoffLexer m T.Text
+quoteChar :: PandocMonad m => Lexer m T.Text
 quoteChar = T.singleton <$> char '"'
 
-mdocToken :: PandocMonad m => RoffLexer m MdocTokens
+mdocToken :: PandocMonad m => Lexer m MdocTokens
 mdocToken = lexComment <|> lexControlLine <|> lexTextLine
 
-lexMacro :: PandocMonad m => RoffLexer m MdocToken
+lexMacro :: PandocMonad m => Lexer m MdocToken
 lexMacro = do
   pos <- getPosition
   name <- many1Char (satisfy isMacroChar)
@@ -307,7 +154,7 @@ lexMacro = do
     isMacroChar '%' = True
     isMacroChar x = isAlphaNum x
 
-lexDelim :: PandocMonad m => RoffLexer m MdocToken
+lexDelim :: PandocMonad m => Lexer m MdocToken
 lexDelim = do
   pos <- getPosition
   t <- Delim Open <$> oneOfStrings ["(", "["] <|> Delim Close <$> oneOfStrings [".", ",", ":", ";", ")", "]", "?", "!"]
@@ -315,7 +162,7 @@ lexDelim = do
   skipSpaces
   return $ t pos
 
-lexLit :: PandocMonad m => RoffLexer m MdocToken
+lexLit :: PandocMonad m => Lexer m MdocToken
 lexLit = do
   pos <- getPosition
   t <- argText <|> quotedArg
@@ -323,7 +170,7 @@ lexLit = do
   skipSpaces
   return $ Lit t pos
 
-lexTextLine :: PandocMonad m => RoffLexer m MdocTokens
+lexTextLine :: PandocMonad m => Lexer m MdocTokens
 lexTextLine = do
   pos <- getPosition
   guard $ sourceColumn pos == 1
@@ -333,7 +180,7 @@ lexTextLine = do
      then return $ singleTok $ Blank pos
      else return $ singleTok $ Str t pos
 
-lexControlLine :: PandocMonad m => RoffLexer m MdocTokens
+lexControlLine :: PandocMonad m => Lexer m MdocTokens
 lexControlLine = do
   pos <- getPosition
   guard $ sourceColumn pos == 1
