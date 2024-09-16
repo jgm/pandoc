6ed3d8387f25d5267aed4b0086159d57f7f799e1
Evan Silberman <evan@jklol.net>
2024-09-15 21:09:39 -0700

    Start Mdoc tests

---
+++ test/Tests/Readers/Mdoc.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE OverloadedStrings #-}
+{- |
+   Module      : Tests.Readers.Mdoc
+   Copyright   : © 2024 Evan Silberman
+   License     : GNU GPL, version 2 or above
+
+   Maintainer  : 
+   Stability   : alpha
+   Portability : portable
+
+Tests for the Mdoc reader.
+-}
+
+module Tests.Readers.Mdoc (tests) where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import Test.Tasty
+import Test.Tasty.HUnit (HasCallStack)
+import Tests.Helpers
+import Text.Pandoc
+import Text.Pandoc.Arbitrary ()
+import Text.Pandoc.Builder
+
+mdoc :: Text -> Pandoc
+mdoc = purely $ readMdoc def
+
+infix 4 =:
+(=:) :: (ToString c, HasCallStack)
+     => String -> (Text, c) -> TestTree
+(=:) = test mdoc
+
+tests :: [TestTree]
+tests = [
+  testGroup "one-line enclosures"
+    [ "Dq" =:
+        ".Dq hello world" =?>
+        para (doubleQuoted "hello world")
+    , "Sq" =:
+        ".Sq hello world" =?>
+        para (singleQuoted "hello world")
+    ]
+  , testGroup "inlines"
+    [ "Sy" =:
+        ".Sy hello world" =?>
+        para (strong "hello world")
+    , "delimiters" =:
+        ".Sy ( hello world )" =?>
+        para (mconcat ["(", strong "hello world", ")"])
+    , "multiple" =:
+        ".Sy hello Em world" =?>
+        para (strong "hello" <> space <> emph "world")
+    ]
+  , testGroup "Ns macro"
+    [ "at the beginning of a macro line (mandoc delta)" =:
+        T.unlines [".Op before", ".Ns Op after"] =?>
+        para "[before][after]"
+    , "after a block closing macro" =:
+        T.unlines [".Oo before", ".Oc Ns Op after"] =?>
+        para "[before][after]"
+    , "in the middle of a macro line" =:
+        ".Oo before Oc Ns Op after" =?>
+        para "[before][after]"
+    , "at the end of a macro line" =:
+        T.unlines [".Oo before Oc Ns", ".Op after"] =?>
+        para "[before][after]"
+    , "at the end of a partial-implicit line" =:
+        T.unlines [".Op before Ns", ".Op after"] =?>
+        para "[before][after]"
+    ]
+  ]
