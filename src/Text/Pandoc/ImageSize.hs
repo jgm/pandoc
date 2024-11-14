b6e3c564a659f51130e5cc57ffc75c6b2940d153
Evan Silberman <evan@jklol.net>
2024-11-14 12:17:11 -0800

    Extract EXIF orientation metadata from images

--- src/Text/Pandoc/ImageSize.hs
+++ src/Text/Pandoc/ImageSize.hs
@@ -14,13 +14,17 @@ Functions for determining the size of a PNG, JPEG, or GIF image.
 -}
 module Text.Pandoc.ImageSize ( ImageType(..)
                              , ImageSize(..)
+                             , ImageTransform(..)
                              , imageType
                              , imageSize
+                             , imageTransform
                              , sizeInPixels
                              , sizeInPoints
                              , desiredSizeInPoints
                              , Dimension(..)
                              , Direction(..)
+                             , Flip(..)
+                             , Rotate(..)
                              , dimension
                              , lengthToDim
                              , scaleDimension
@@ -41,6 +45,7 @@ import Data.Bits ((.&.), shiftR, shiftL)
 import Data.Word (bitReverse32)
 import Data.Maybe (isJust, fromJust)
 import Data.Char (isDigit)
+import Data.Maybe (fromMaybe)
 import Control.Monad
 import Text.Pandoc.Shared (safeRead)
 import Data.Default (Default)
@@ -56,6 +61,7 @@ import Control.Applicative
 import qualified Data.Attoparsec.ByteString as AW
 import qualified Data.Attoparsec.ByteString.Char8 as A
 import qualified Codec.Picture.Metadata as Metadata
+import Codec.Picture.Metadata.Exif (ExifTag(TagOrientation), ExifData(..))
 import Codec.Picture (decodeImageWithMetadata)
 
 -- quick and dirty functions to get image sizes
@@ -83,6 +89,26 @@ instance Show Dimension where
   show (Inch a)       = T.unpack (showFl a) ++ "in"
   show (Percent a)    = show a              ++ "%"
   show (Em a)         = T.unpack (showFl a) ++ "em"
+
+data Flip = NoFlip | FlipH | FlipV deriving Show
+
+data Rotate = R0 | R90 | R180 | R270 deriving Show
+
+-- There's a case to be made that this API is wrong and other code should deal
+-- somewhat more directly with the 8 EXIF orientations that actually occur,
+-- because as-is we could model transformations that don't occur in nature,
+-- like FlipV + R270. But since mirroring and rotating are distinct operations
+-- everywhere we happen to deal with it, I don't think anything is gained in
+-- practice by making ImageTransform a sum type with 8 fixed cases instead of a
+-- product type that theoretically allows for 4 cases we won't encounter.
+data ImageTransform = ImageTransform
+  { tFlip :: Flip,
+    tRotate :: Rotate
+  }
+  deriving (Show)
+
+instance Default ImageTransform where
+  def = ImageTransform NoFlip R0
 
 data ImageSize = ImageSize{
                      pxX   :: Integer
@@ -451,3 +477,24 @@ webpSize opts img =
   case AW.parseOnly pWebpSize img of
     Left _   -> Nothing
     Right sz -> Just sz { dpiX = fromIntegral $ writerDpi opts, dpiY = fromIntegral $ writerDpi opts}
+
+imageTransform :: ByteString -> ImageTransform
+imageTransform img =
+  case decodeImageWithMetadata img of
+    Left _ -> def
+    Right (_, meta) ->
+      let orient = fromMaybe ExifNone $ Metadata.lookup (Metadata.Exif TagOrientation) meta
+       in exifToTransform (word orient)
+  where
+    word ExifNone = 1
+    word (ExifShort w) = w
+    word _ = 1
+    exifToTransform 1 = def
+    exifToTransform 2 = def{tFlip = FlipH}
+    exifToTransform 3 = def{tRotate = R180}
+    exifToTransform 4 = def{tFlip = FlipV}
+    exifToTransform 5 = def{tFlip = FlipH, tRotate = R270}
+    exifToTransform 6 = def{tRotate = R90}
+    exifToTransform 7 = def{tFlip = FlipH, tRotate = R90}
+    exifToTransform 8 = def{tRotate = R270}
+    exifToTransform _ = def
