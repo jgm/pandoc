c5461628714f1d529291de87498404e0cef5dc4c
Stephen Reindl <stephen.reindl@outlook.de>
2025-01-29 19:05:01 +0100

    flatten Div sections if `+styles` is provided

--- src/Text/Pandoc/Readers/Docx.hs
+++ src/Text/Pandoc/Readers/Docx.hs
@@ -58,6 +58,7 @@ implemented, [-] means partially implemented):
 module Text.Pandoc.Readers.Docx
        ( readDocx
        ) where
+
 import Codec.Archive.Zip
 import Control.Monad ( liftM, unless )
 import Control.Monad.Reader
@@ -356,10 +357,10 @@ extentToAttr _ = nullAttr
 blocksToInlinesWarn :: PandocMonad m => T.Text -> Blocks -> DocxContext m Inlines
 blocksToInlinesWarn cmtId blks = do
   let paraOrPlain :: Block -> Bool
-      paraOrPlain (Para _)  = True
-      paraOrPlain (Plain _) = True
-      paraOrPlain (Div _ _) = True
-      paraOrPlain _         = False
+      paraOrPlain (Para _)       = True
+      paraOrPlain (Plain _)      = True
+      paraOrPlain (Div _ nested) = (all paraOrPlain nested)
+      paraOrPlain _              = False
   unless (all paraOrPlain blks) $
     lift $ P.report $ DocxParserWarning $
       "Docx comment " <> cmtId <> " will not retain formatting"
