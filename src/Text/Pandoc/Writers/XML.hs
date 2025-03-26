9f8fb285a4e31d94be425131a9eba6e0b975a9c2
massifrg <massifrg@gmail.com>
2025-03-26 19:24:17 +0100

    wip
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip

--- src/Text/Pandoc/Writers/XML.hs
+++ src/Text/Pandoc/Writers/XML.hs
@@ -4,40 +4,47 @@
 
 module Text.Pandoc.Writers.XML (writeXML) where
 
--- import Text.Pandoc
-
 import Data.Aeson
 import qualified Data.Aeson.Key as K
 import qualified Data.Aeson.KeyMap as KM
-import Data.Maybe (isJust)
+import Data.Maybe (mapMaybe)
+import Data.Scientific (FPFormat (Fixed), Scientific, formatScientific, isInteger)
 import qualified Data.Text as T
+import qualified Data.Vector as V
 import Text.Pandoc.Class.PandocMonad (PandocMonad)
 import Text.Pandoc.Definition
 import Text.Pandoc.Options (WriterOptions (..))
-import Text.Pandoc.XML (escapeStringForXML, inTagsSimple)
 import Text.Pandoc.XML.Light
 import qualified Text.Pandoc.XML.Light as XML
 
-data ContentsOrAttrs
-  = Contents Content
-  | Attrs [XML.Attr]
-  deriving (Show, Eq)
+data Context
+  = None
+  | PandocObject
+  | MetaData
+  | BlockElem
+  | InlineElem
+  | Attribute T.Text
+  | AttrData
+  | WordsAttribute T.Text
+  | Attributes
+  | ListElem
+  | ListAttributes
+  | ListContents
+  | ListItem
+  | TextData
+  deriving (Eq, Show)
 
-pandocElement :: Element
-pandocElement =
-  Element
-    { elName = unqual "Pandoc", -- Tag name
-      elAttribs = [XML.Attr (unqual "class") "my-class"], -- Attributes
-      elContent = [Text (CData CDataText "Hello, World!" Nothing)], -- Child content
-      elLine = Nothing -- Line number (optional, usually `Nothing`)
-    }
+data ElementType
+  = IsBlockOfBlocks
+  | IsBlockOfInlines
+  | IsInline
+  | IsListElem
+  | IsLineBlockElem
+  deriving (Eq, Show)
 
 writeXML :: (PandocMonad m) => WriterOptions -> Pandoc -> m T.Text
 writeXML _ doc = do
-  let maybeXml = valueToXML pandocElement (toJSON doc)
-  case maybeXml of
-    (Contents (Elem xml)) -> return $ showTopElement xml
-    _ -> return ""
+  return $ showTopElement $ processValue (empty_element "Pandoc") PandocObject $ toJSON doc
 
 text_node :: T.Text -> Content
 text_node text = Text (CData CDataText text Nothing)
@@ -45,128 +52,213 @@ text_node text = Text (CData CDataText text Nothing)
 empty_text :: Content
 empty_text = text_node ""
 
-valueToXML :: Element -> Value -> ContentsOrAttrs
-valueToXML _ (Object obj) =
-  if (isPandocObject (Object obj))
-    then Contents $ Elem pandocElement
-    else Contents $ text_node ""
-valueToXML _ (Array _) = Contents empty_text
-valueToXML _ _ = Contents empty_text
+empty_element :: T.Text -> Element
+empty_element tag =
+  Element
+    { elName = unqual tag,
+      elAttribs = [],
+      elContent = [],
+      elLine = Nothing
+    }
 
--- process a JSON Object with "t" and maybe "c"
-typedValueToXML :: T.Text -> Maybe Value -> ContentsOrAttrs
-typedValueToXML "Str" (Just (String text)) = Contents $ text_node $ escapeStringForXML text
-typedValueToXML "Space" Nothing = Contents $ text_node " "
-typedValueToXML tag Nothing = Contents $ text_node $ mconcat ["<", tag, " />"]
--- typedValueToXML t (Just (Array a)) =
-typedValueToXML _ _ = Contents empty_text
+element_name :: Element -> T.Text
+element_name element = qName $ elName element
 
-objectType :: Value -> Maybe T.Text
-objectType (Object obj) = case KM.lookup "t" obj of
-  Just (String tValue) -> Just tValue
-  _ -> Nothing
-objectType _ = Nothing
+scientific_to_text :: Scientific -> T.Text
+scientific_to_text sc
+  | isInteger sc = T.pack (show (round sc :: Integer)) -- No decimals if it's an integer
+  | otherwise = T.pack (formatScientific Fixed Nothing sc) -- Format with Fixed otherwise
 
-objectHasKey :: T.Text -> Value -> Bool
-objectHasKey key (Object obj) = isJust (KM.lookup (K.fromText key) obj)
-objectHasKey _ _ = False
+appendContentsToElement :: Element -> [Content] -> Element
+appendContentsToElement el newContents = el {elContent = (elContent el) ++ newContents}
 
-isPandocObject :: Value -> Bool
-isPandocObject value =
-  (objectHasKey "blocks" value)
-    && (objectHasKey "meta" value)
-    && (objectHasKey "pandoc-api-version" value)
+addAttributesToElement :: Element -> [XML.Attr] -> Element
+addAttributesToElement el newAttrs = el {elAttribs = newAttrs ++ elAttribs el}
 
-objectHasType :: Value -> Bool
-objectHasType value = objectHasKey "t" value
+arrayAtKey :: Object -> T.Text -> Array
+arrayAtKey obj key = case (KM.lookup (K.fromText key) obj) of
+  Just (Array arr) -> arr
+  _ -> V.empty
 
-objectHasContent :: Value -> Bool
-objectHasContent value = objectHasKey "c" value
+objectAtKey :: Object -> T.Text -> Object
+objectAtKey obj key = case (KM.lookup (K.fromText key) obj) of
+  Just (Object o) -> o
+  _ -> KM.empty
 
-objectHasTypeAndContent :: Value -> Bool
-objectHasTypeAndContent value = (objectHasContent value) && (objectHasType value)
+processPandocObject :: Element -> Object -> Element
+processPandocObject element obj =
+  let meta = Elem $ processObject (empty_element "meta") MetaData $ objectAtKey obj "meta"
+      blocks = newElementWithContents "blocks" [BlockElem] $ arrayAtKey obj "blocks"
+   in -- add apiVersion attribute
+      appendContentsToElement element [meta, blocks]
 
--- processContents :: Array -> Element -> Element
+newElementWithContents :: T.Text -> [Context] -> Array -> Content
+newElementWithContents tag contexts contents =
+  Elem $ processArray (empty_element tag) contexts $ V.toList contents
 
--- processContent :: Value -> Element -> Element
+-- process Object that has the "t" key
+processObject :: Element -> Context -> Object -> Element
+processObject element context obj = case (context, t, c) of
+  (PandocObject, _, Nothing) -> processPandocObject element obj
+  (_, "Str", Just (String (text))) -> appendContentsToElement element [text_node text]
+  (_, "Space", Nothing) -> appendContentsToElement element [text_node " "]
+  -- (_, "", _, Just (Array (contents))) -> processArray element [context] $ V.toList contents -- should not happen
+  (_, _, Just (Array (contents))) -> processObjectWithArrayContents element t contents
+  (Attribute ("col-width"), "ColWidthDefault", Nothing) -> addAttributesToElement element [Attr (unqual "col-width") "0"]
+  (Attribute (name), _, Nothing) -> addAttributesToElement element [Attr (unqual name) t]
+  (_, tag, Nothing) -> appendContentsToElement element [Elem $ empty_element tag] -- HorizontalRule, SoftBreak, LineBreak
+  _ -> element
+  where
+    t = case KM.lookup (K.fromText "t") obj of
+      Just (String (text)) -> text
+      _ -> ""
+    c = KM.lookup (K.fromText "c") obj
 
--- Element "Pandoc" [] [] Nothing
--- jsonToXML (Object obj) =
---     let tVal = obj .: "t"
---         cVal = obj .: "c"
---     in case tVal of
---         Just (String "Space") -> Element "Space" mempty [NodeContent " "]
---         Just (String "Str") -> Element "Str" mempty [NodeContent (extractText cVal)]
---         Just (String "Emph") -> Element "Emph" mempty (convertChildren cVal)
---         _ -> Element "Unknown" mempty []  -- Fallback for unsupported types
--- jsonToXML _ = Element "Invalid" mempty []
+processObjectWithArrayContents :: Element -> T.Text -> Array -> Element
+processObjectWithArrayContents el tag contents = case (tag, el_type) of
+  (_, IsBlockOfInlines) -> createElemAndProcessValuesAs [InlineElem] -- Plain, Para
+  (_, IsBlockOfBlocks) -> createElemAndProcessValuesAs [BlockElem] -- BlockQuote
+  ("Div", _) -> createElemAndProcessValuesAs [AttrData, BlockElem]
+  ("Span", _) -> createElemAndProcessValuesAs [AttrData, InlineElem]
+  ("Header", _) -> createElemAndProcessValuesAs [Attribute "level", AttrData, InlineElem]
+  ("BulletList", _) -> createElemAndProcessValuesAs [ListContents]
+  ("OrderedList", _) -> createElemAndProcessValuesAs [ListAttributes, ListContents]
+  _ -> createElemAndProcessValuesAs [BlockElem]
+  where
+    el_type = elementType el
+    createElemAndProcessValuesAs :: [Context] -> Element
+    createElemAndProcessValuesAs contexts = appendContentsToElement el [newElementWithContents tag contexts contents]
 
--- Helper to extract Text content (assuming cVal is an array)
--- extractText :: Maybe Value -> T.Text
--- extractText (Just (String text)) = text
--- extractText _ = T.empty
+processValue :: Element -> Context -> Value -> Element
+processValue el context v = case (context, v) of
+  (_, Object (obj)) -> processObject el context obj
+  (AttrData, Array (arr)) -> processAttr el $ V.toList arr
+  (Attribute attr_name, Number (number)) -> addAttributesToElement el [Attr (unqual attr_name) (scientific_to_text number)]
+  (Attribute attr_name, String (text)) -> addAttributesToElement el [Attr (unqual attr_name) text]
+  (WordsAttribute attr_name, Array (wrds)) -> addAttributesToElement el [Attr (unqual attr_name) $ wordsAttrValue (V.toList wrds)]
+  (Attributes, Array (a)) -> addAttributesToElement el $ arrayToAttributes $ V.toList a
+  (ListAttributes, Array (a)) -> processArray el [Attribute "start", Attribute "number-style", Attribute "number-delim"] $ V.toList a
+  (ListContents, Array (a)) -> appendContentsToElement el $ wrapItems "item" BlockElem $ V.toList a
+  (ListItem, Array (a)) -> appendContentsToElement el [newElementWithContents "item" [BlockElem] a]
+  (_, Array (a)) -> processArray el [context] $ V.toList a
+  (_, String (text)) -> appendContentsToElement el [text_node text]
+  _ -> el
 
--- -- Handle child nodes (e.g., arrays of Inline or Block)
--- convertChildren :: Maybe Value -> [Text.XML.Light.Element]
--- convertChildren (Just (Array arr)) = map jsonToXML (V.toList arr)
--- convertChildren _ = []
+wrapItems :: T.Text -> Context -> [Value] -> [Content]
+wrapItems tag context values = map wrapInElement values
+  where
+    wrapInElement :: Value -> Content
+    wrapInElement (Array a) = Elem $ processArray item_el [context] $ V.toList a
+      where
+        item_el = empty_element tag
+    wrapInElement _ = empty_text
 
--- -- Check if a Value is a Pandoc Object with "blocks", "pandoc-api-version", and "meta" keys
--- isPandocObject :: Value -> Bool
--- isPandocObject = withObject "PandocObject" $ \obj -> do
---     _ <- obj .: "blocks"             -- Access "blocks"
---     _ <- obj .: "pandoc-api-version" -- Access "pandoc-api-version"
---     _ <- obj .: "meta"               -- Access "meta"
---     return True
+processArray :: Element -> [Context] -> [Value] -> Element
+processArray element contexts contents = case (contents) of
+  [] -> element
+  x : xs -> processArray (processValue element (head contexts) x) contexts_tail xs
+    where
+      contexts_tail = case (contexts) of
+        [] -> [None]
+        [c] -> [c]
+        _ : cs -> cs
 
--- -- Extract the fields for "blocks", "pandoc-api-version", and "meta" using the `Parser` monad
--- extractPandocFields :: Value -> Parser (Value, Value, Value)
--- extractPandocFields = withObject "PandocObject" $ \obj -> do
---     blocks <- obj .: "blocks"
---     version <- obj .: "pandoc-api-version"
---     meta <- obj .: "meta"
---     return (blocks, version, meta)
+elementType :: Element -> ElementType
+elementType el = tagType $ element_name el
 
--- docToXML :: Pandoc -> Element
--- docToXML (Pandoc meta blocks) = unode "Pandoc" [ (metaElement meta), (blocksElement blocks) ]
+tagType :: T.Text -> ElementType
+tagType name = case (name) of
+  "Str" -> IsInline
+  "Space" -> IsInline
+  "Para" -> IsBlockOfInlines
+  "Header" -> IsBlockOfInlines
+  "Plain" -> IsBlockOfInlines
+  "Emph" -> IsInline
+  "Underline" -> IsInline
+  "Strong" -> IsInline
+  "Strikeout" -> IsInline
+  "Superscript" -> IsInline
+  "Subscript" -> IsInline
+  "SmallCaps" -> IsInline
+  "Quoted" -> IsInline
+  "Cite" -> IsInline
+  "Code" -> IsInline
+  "SoftBreak" -> IsInline
+  "LineBreak" -> IsInline
+  "Math" -> IsInline
+  "RawInline" -> IsInline
+  "Link" -> IsInline
+  "Image" -> IsInline
+  "Note" -> IsInline
+  "Span" -> IsInline
+  "BulletList" -> IsListElem
+  "OrderedList" -> IsListElem
+  "LineBlock" -> IsLineBlockElem
+  "line" -> IsBlockOfInlines
+  _ -> IsBlockOfBlocks
 
--- metaElement :: Meta -> Element
--- metaElement (meta) = unode "meta" ([] :: [Content]) -- TODO: convert single meta values
+wordsAttrValue :: [Value] -> T.Text
+wordsAttrValue wrds =
+  if areAllStrings wrds
+    then
+      T.unwords $ map extractText wrds
+    else ""
+  where
+    extractText (String t) = t
+    extractText _ = T.empty
+    areAllStrings values = not (null values) && (all valueIsString values)
 
--- blocksElement :: [Block] -> Element
--- blocksElement (blocks) = unode "blocks" (map blockToXML blocks)
+arrayToAttributes :: [Value] -> [XML.Attr]
+arrayToAttributes values = mapMaybe valueToAttr values
+  where
+    valueToAttr :: Value -> Maybe XML.Attr
+    valueToAttr (Array arr')
+      | V.length arr' == 2 =
+          case V.toList arr' of
+            ["id", _] -> Nothing
+            ["class", _] -> Nothing
+            [String name, String value] -> Just (Attr (unqual name) value)
+            _ -> Nothing
+      | otherwise = Nothing
+    valueToAttr _ = Nothing
 
--- mkCData :: T.Text -> [Content]
--- mkCData text = [Elem $ blank_element { elContent = [CRef $ T.unpack text] }]
+processAttr :: Element -> [Value] -> Element
+processAttr element arr = case (arr) of
+  [String identifier, Array classes, Array attributes] ->
+    let withId = addIdToElement element identifier
+        withClasses = addClassesToElement withId (V.toList classes)
+        withAttributes = addAttributesToElement withClasses (valuesToAttrs $ V.toList attributes)
+     in withAttributes
+  _ -> element
+  where
+    addIdToElement :: Element -> T.Text -> Element
+    addIdToElement el identifier = case (identifier) of
+      "" -> el
+      value -> addAttributesToElement el [Attr (unqual "id") value]
 
--- textToCData :: T.Text -> CData
--- textToCData text = blank_cdata { cdData = T.unpack text }
+    addClassesToElement :: Element -> [Value] -> Element
+    addClassesToElement el classes =
+      if areAllStrings classes
+        then
+          addAttributesToElement el [Attr (unqual "class") (T.unwords $ map extractText classes)]
+        else el
+      where
+        extractText (String t) = t
+        extractText _ = T.empty
+        areAllStrings values = not (null values) && (all valueIsString values)
 
--- extractFormatName :: Format -> String
--- extractFormatName (Format fmt) = T.unpack fmt
+    valueToAttr :: Value -> Maybe XML.Attr
+    valueToAttr (Array arr')
+      | V.length arr' == 2 =
+          case V.toList arr' of
+            [String name, String value] -> Just (Attr (unqual name) value)
+            _ -> Nothing
+      | otherwise = Nothing
+    valueToAttr _ = Nothing
 
--- blockToXML :: Block -> Element
--- blockToXML blk = case blk of
---   Para inlines -> unode "Para" (map inlineToContent inlines)
---   Plain inlines -> unode "Plain" (map inlineToContent inlines)
---   Header _ _ inlines -> unode "Header" (map inlineToContent inlines)
---   CodeBlock _ text -> unode "CodeBlock" (mkCData text)
---   RawBlock format text -> unode "RawBlock" ([rawAttr format], (textToCData text))
---   _ -> unode "Unknown" ()
+    valuesToAttrs :: [Value] -> [XML.Attr]
+    valuesToAttrs values = mapMaybe valueToAttr values
 
--- inlineToContent :: Inline -> Content
--- inlineToContent inl = case inl of
---   Space -> Text $ textToCData " "
---   Str text -> Text $ textToCData text
---   Emph inlines -> Elem $ unode "Emph" (map inlineToContent inlines)
---   Strong inlines -> Elem $ unode "Strong" (map inlineToContent inlines)
---   SmallCaps inlines -> Elem $ unode "SmallCaps" (map inlineToContent inlines)
---   Superscript inlines -> Elem $ unode "Superscript" (map inlineToContent inlines)
---   Subscript inlines -> Elem $ unode "Subscript" (map inlineToContent inlines)
---   Underline inlines -> Elem $ unode "Underline" (map inlineToContent inlines)
---   Code _ text -> Elem $ unode "Code" (textToCData text)
---   RawInline format text -> Elem $ unode "RawInline" ([rawAttr format], (textToCData text))
---   _ -> Elem $ unode "Unknown" ()
-
--- rawAttr :: Format -> Text.XML.Light.Attr
--- rawAttr format = Attr (unqual "format") (extractFormatName format)
+valueIsString :: Value -> Bool
+valueIsString (String _) = True
+valueIsString _ = False
