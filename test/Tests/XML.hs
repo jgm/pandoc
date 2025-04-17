36eaef1d8e9022cdad73b1cddc20ebae741ad00f
massifrg <massifrg@gmail.com>
2025-04-17 18:19:14 +0200

    xml test: filter out ill docs

--- test/Tests/XML.hs
+++ test/Tests/XML.hs
@@ -2,13 +2,13 @@
 
 module Tests.XML (tests) where
 
+import Data.Text (unpack)
 import Test.Tasty (TestTree)
 import Test.Tasty.QuickCheck
 import Tests.Helpers
 import Text.Pandoc
 import Text.Pandoc.Arbitrary ()
 import Text.Pandoc.Walk (query)
-import System.Process (terminateProcess)
 
 p_xml_roundtrip :: Pandoc -> Property
 p_xml_roundtrip d = isValidPandoc d ==> d' == d
@@ -17,36 +17,55 @@ p_xml_roundtrip d = isValidPandoc d ==> d' == d
     d' = purely (readXML def) xml
 
 isValidPandoc :: Pandoc -> Bool
-isValidPandoc d = not (hasEmptyStr || hasSuccStrs || hasSuccSpaces)
+isValidPandoc d = not (hasEmptyStr || hasSuccSameInline || hasSuccSpaces || hasSpaceAroundBreaks)
   where
     inlines = (query extractInlines d) ++ (query extractMetaInlines d)
-    hasEmptyStr = any (any (\i -> i == Str "")) inlines
-    hasSuccStrs = any  hasSuccessiveStr inlines
+    hasEmptyStr = any (any isIllStr) inlines
+    hasSuccSameInline = any hasSuccessiveInline inlines
     hasSuccSpaces = any hasMultipleSpace inlines
+    hasSpaceAroundBreaks = any hasSpaceAroundBreak inlines
 
-hasSuccessiveStr :: [Inline] -> Bool
-hasSuccessiveStr ((Str _) : (Str _) : _) = True
-hasSuccessiveStr (_ : xs) = hasSuccessiveStr xs
-hasSuccessiveStr [] = False
+-- a Str is ill if its text is empty or it contains spaces
+isIllStr :: Inline -> Bool
+isIllStr (Str s) = s == "" || (' ' `elem` (unpack s))
+isIllStr _ = False
 
+-- detect two consecutive Inlines of the same type in a [Inline]
+hasSuccessiveInline :: [Inline] -> Bool
+hasSuccessiveInline ((Str _) : (Str _) : _) = True
+hasSuccessiveInline ((Emph _) : (Emph _) : _) = True
+hasSuccessiveInline ((Strong _) : (Strong _) : _) = True
+hasSuccessiveInline ((Underline _) : (Underline _) : _) = True
+hasSuccessiveInline ((SmallCaps _) : (SmallCaps _) : _) = True
+hasSuccessiveInline ((Strikeout _) : (Strikeout _) : _) = True
+hasSuccessiveInline ((Subscript _) : (Subscript _) : _) = True
+hasSuccessiveInline ((Superscript _) : (Superscript _) : _) = True
+hasSuccessiveInline ((Quoted q1 _) : (Quoted q2 _) : _) = q1 == q2
+-- hasSuccessiveInline ((Math mt1 _) : (Math mt2 _) : _) = mt1 == mt2
+-- hasSuccessiveInline ((RawInline f1 _) : (RawInline f2 _) : _) = f1 == f2
+hasSuccessiveInline (_ : xs) = hasSuccessiveInline xs
+hasSuccessiveInline [] = False
+
+-- detect two consecutive Space in a [Inline]
 hasMultipleSpace :: [Inline] -> Bool
 hasMultipleSpace (Space : Space : _) = True
 hasMultipleSpace (_ : xs) = hasMultipleSpace xs
 hasMultipleSpace [] = False
 
+hasSpaceAroundBreak :: [Inline] -> Bool
+hasSpaceAroundBreak (Space : SoftBreak : _) = True
+hasSpaceAroundBreak (SoftBreak : Space : _) = True
+hasSpaceAroundBreak (LineBreak : Space : _) = True
+hasSpaceAroundBreak (Space : LineBreak : _) = True
+hasSpaceAroundBreak (_ : xs) = hasSpaceAroundBreak xs
+hasSpaceAroundBreak _ = False
+
 extractMetaInlines :: MetaValue -> [[Inline]]
 extractMetaInlines (MetaInlines inlines) = [inlines]
 extractMetaInlines _ = []
 
-extractInlines :: Block  -> [[Inline]]
-extractInlines (Para inlines) = [inlines]
-extractInlines (Plain inlines) = [inlines]
-extractInlines (Header _ _ inlines) = [inlines]
-extractInlines (Figure _ (Caption (Just (inlines)) _) _) = [inlines]
-extractInlines (Table _ (Caption (Just (inlines)) _) _ _ _ _) = [inlines]
-extractInlines (LineBlock blines) = blines
-extractInlines (DefinitionList items) = map fst items
-extractInlines _ = []
+extractInlines :: [Inline] -> [[Inline]]
+extractInlines inlines = [inlines]
 
 tests :: [TestTree]
 tests = [testProperty "p_xml_roundtrip" p_xml_roundtrip]
