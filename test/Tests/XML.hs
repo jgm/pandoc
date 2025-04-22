2837dd791639f891d861e97f1b0463d6fcd67a72
massifrg <massifrg@gmail.com>
2025-04-22 16:21:25 +0200

    xml format test passes with normalization
    No test document is discarded, but there's a normalizazion that corrects Str "", Str followed by another Str and Str containing spaces.
    It also removes empty Plain, Para, Header and list items.
    All previous Inline normalizations (consecutive breaks, spaces around breaks, consecutive inlines like Emph) are no longer needed, after using [concatMany](https://github.com/jgm/pandoc/pull/10784#issuecomment-2816358821)

--- test/Tests/XML.hs
+++ test/Tests/XML.hs
@@ -2,23 +2,20 @@
 
 module Tests.XML (tests) where
 
-import Data.Text (unpack)
+import Control.Monad ((>=>))
+import Data.Text as T hiding (reverse)
 import Test.Tasty (TestTree)
 import Test.Tasty.QuickCheck
 import Tests.Helpers
 import Text.Pandoc
 import Text.Pandoc.Arbitrary ()
 import Text.Pandoc.Builder as B
-import Text.Pandoc.Walk (query, walk)
-import Control.Monad ((>=>))
+import Text.Pandoc.Walk (walk)
 
 p_xml_roundtrip :: Property
 p_xml_roundtrip = forAll (normalize <$> arbitrary) testdoc
   where
-    testdoc d =
-      if isValidPandoc d
-        then d == purely (writeXML def >=> readXML def) d
-        else discard
+    testdoc d = d == purely (writeXML def >=> readXML def) d
 
 normalize :: Pandoc -> Pandoc
 normalize d =
@@ -37,54 +34,39 @@ normalize' = walk simplify . (walk simplifyBlocks)
 simplifyInlines :: [Inline] -> [Inline]
 simplifyInlines (Str "" : xs) = simplifyInlines xs
 simplifyInlines (Space : Space : xs) = simplifyInlines $ Space : xs
-simplifyInlines (Str s1 : Str s2 : xs) = simplifyInlines $ Str (s1 <> s2) : xs
-simplifyInlines (Space : SoftBreak : xs) = simplifyInlines $ SoftBreak : xs
-simplifyInlines (Space : LineBreak : xs) = simplifyInlines $ LineBreak : xs
-simplifyInlines (SoftBreak : Space : xs) = simplifyInlines $ SoftBreak : xs
-simplifyInlines (LineBreak : Space : xs) = simplifyInlines $ LineBreak : xs
-simplifyInlines (LineBreak : SoftBreak : xs) = simplifyInlines $ LineBreak : xs
-simplifyInlines (SoftBreak : LineBreak : xs) = simplifyInlines $ LineBreak : xs
-simplifyInlines (SoftBreak : SoftBreak : xs) = simplifyInlines $ SoftBreak : xs
-simplifyInlines (Emph ils1 : Emph ils2 : xs) = simplifyInlines $ Emph (ils1 ++ ils2) : xs
-simplifyInlines (Strong ils1 : Strong ils2 : xs) = simplifyInlines $ Strong (ils1 ++ ils2) : xs
-simplifyInlines (Superscript ils1 : Superscript ils2 : xs) = simplifyInlines $ Superscript (ils1 ++ ils2) : xs
-simplifyInlines (Subscript ils1 : Subscript ils2 : xs) = simplifyInlines $ Subscript (ils1 ++ ils2) : xs
-simplifyInlines (Strikeout ils1 : Strikeout ils2 : xs) = simplifyInlines $ Strikeout (ils1 ++ ils2) : xs
-simplifyInlines (Underline ils1 : Underline ils2 : xs) = simplifyInlines $ Strikeout (ils1 ++ ils2) : xs
-simplifyInlines (SmallCaps ils1 : SmallCaps ils2 : xs) = simplifyInlines $ SmallCaps (ils1 ++ ils2) : xs
+simplifyInlines (Str s1 : Str s2 : xs) = simplifyInlines $ strsAndSpaces (s1 <> s2) ++ xs
+simplifyInlines (Str s : xs) = (strsAndSpaces s) ++ simplifyInlines xs
 simplifyInlines (x : xs) = x : simplifyInlines xs
 simplifyInlines [] = []
 
+strsAndSpaces :: Text -> [Inline]
+strsAndSpaces s =
+  if T.any (== ' ') s
+    then to_inlines (T.split (== ' ') s) []
+    else [Str s]
+  where
+    to_inlines :: [Text] -> [Inline] -> [Inline]
+    to_inlines ("" : xs) [] = to_inlines xs [Space]
+    to_inlines ("" : xs) (Space : ilss) = to_inlines xs (Space : ilss)
+    to_inlines ("" : xs) ils = to_inlines xs (Space : ils)
+    to_inlines (x : xs) [] = to_inlines xs [Str x]
+    to_inlines (x : xs) (Space : ilss) = to_inlines xs (Str x : Space : ilss)
+    to_inlines (x : xs) ils = to_inlines xs (Str x : Space : ils)
+    to_inlines [] ils = reverse ils
+
 simplifyBlocks :: [Block] -> [Block]
-simplifyBlocks (Plain []: xs) = simplifyBlocks xs
-simplifyBlocks (Para []: xs) = simplifyBlocks xs
-simplifyBlocks (Header _ _ []: xs) = simplifyBlocks xs
-simplifyBlocks (x:xs ) = x : simplifyBlocks xs
+simplifyBlocks (Plain [] : xs) = simplifyBlocks xs
+simplifyBlocks (Para [] : xs) = simplifyBlocks xs
+simplifyBlocks (Header _ _ [] : xs) = simplifyBlocks xs
+simplifyBlocks (BulletList items : xs) = BulletList (removeEmptyItems items) : simplifyBlocks xs
+simplifyBlocks (OrderedList la items : xs) = OrderedList la (removeEmptyItems items) : simplifyBlocks xs
+simplifyBlocks (x : xs) = x : simplifyBlocks xs
 simplifyBlocks [] = []
 
-isValidPandoc :: Pandoc -> Bool
-isValidPandoc d = not has_ilnesses
-  where
-    has_ilnesses = hasEmptyStr
-    inlines = (query extractInlines d) ++ (query extractMetaInlines d)
-    hasEmptyStr = any (any isIllStr) inlines
-
--- hasSpaceAroundBreaks = any hasSpaceAroundBreak inlines
--- hasSuccSpaces = any hasMultipleSpace inlines
--- hasSuccSameInline = any hasSuccessiveInline inlines
-
--- a Str is ill if its text is empty or it contains spaces
-isIllStr :: Inline -> Bool
--- isIllStr (Str s) = s == "" || (' ' `elem` (unpack s))
-isIllStr (Str s) = ' ' `elem` (unpack s)
-isIllStr _ = False
-
-extractMetaInlines :: MetaValue -> [[Inline]]
-extractMetaInlines (MetaInlines inlines) = [inlines]
-extractMetaInlines _ = []
-
-extractInlines :: [Inline] -> [[Inline]]
-extractInlines inlines = [inlines]
+removeEmptyItems :: [[Block]] -> [[Block]]
+removeEmptyItems ([] : xs) = removeEmptyItems xs
+removeEmptyItems (x : xs) = x : removeEmptyItems xs
+removeEmptyItems [] = []
 
 tests :: [TestTree]
 tests = [testProperty "p_xml_roundtrip" p_xml_roundtrip]
