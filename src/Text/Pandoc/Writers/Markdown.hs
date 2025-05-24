jgm/pandoc

commit 0d2114ee4e3974583ee84e8a4e992562d6502f66

Markdown writer: improve use of implicit figures when possible.
Closes #10758.

When the alt differs from the caption, but only as regards
formatting, we still use an implicit figure.

src/Text/Pandoc/Writers/Markdown.hs
@@ -709,16 +709,20 @@ blockToMarkdown' opts (DefinitionList items) = do
   return $ mconcat contents <> blankline
 blockToMarkdown' opts (Figure figattr capt body) = do
   let combinedAttr imgattr = case imgattr of
-        ("", cls, kv) | (figid, [], []) <- figattr -> Just (figid, cls, kv)
+        ("", cls, kv)
+          | (figid, [], []) <- figattr -> Just (figid, cls, kv)
+          | otherwise -> Just ("", cls, kv)
         _ -> Nothing
   let combinedAlt alt = case capt of
         Caption Nothing [] -> if null alt
                               then Just [Str "image"]
                               else Just alt
         Caption Nothing [Plain captInlines]
-          | captInlines == alt || null alt -> Just captInlines
+          | null alt || stringify captInlines == stringify alt
+            -> Just captInlines
         Caption Nothing [Para captInlines]
-          | captInlines == alt || null alt -> Just captInlines
+          | null alt || stringify captInlines == stringify alt
+            -> Just captInlines
         _ -> Nothing
   case body of
     [Plain [Image imgAttr alt (src, ttl)]]

commit fad45a67f93fc8386049c23dff7e3b2bbbaf0266

Markdown writer: preserve figure attributes
0d2114e introduced a bug where Markdown writer would ignore attributes
on the figure if it has class or key-value attributes set.

This commit fixes that and adds a regression test.

Closes #10867

src/Text/Pandoc/Writers/Markdown.hs
@@ -721,7 +721,6 @@ blockToMarkdown' opts (Figure figattr capt body) = do
   let combinedAttr imgattr = case imgattr of
         ("", cls, kv)
           | (figid, [], []) <- figattr -> Just (figid, cls, kv)
-          | otherwise -> Just ("", cls, kv)
         _ -> Nothing
   let combinedAlt alt = case capt of
         Caption Nothing [] -> if null alt