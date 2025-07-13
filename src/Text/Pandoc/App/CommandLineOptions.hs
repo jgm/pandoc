9d7996ea2b7c04b0a327eb5aca7538cf4c899a16
John MacFarlane <jgm@berkeley.edu>
2025-07-13 09:05:53 -0700

    Update `--version` copyright dates.
    Closes #10961.

--- src/Text/Pandoc/App/CommandLineOptions.hs
+++ src/Text/Pandoc/App/CommandLineOptions.hs
@@ -1172,7 +1172,7 @@ usageMessage programName = usageInfo (programName ++ " [OPTIONS] [FILES]")
 
 copyrightMessage :: String
 copyrightMessage = intercalate "\n" [
- "Copyright (C) 2006-2024 John MacFarlane. Web:  https://pandoc.org",
+ "Copyright (C) 2006-2025 John MacFarlane. Web:  https://pandoc.org",
  "This is free software; see the source for copying conditions. There is no",
  "warranty, not even for merchantability or fitness for a particular purpose." ]
 
