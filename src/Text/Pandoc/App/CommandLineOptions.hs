ee4c9352ee9282f348d9459eafc08cbc0c671a91
John MacFarlane <jgm@berkeley.edu>
2025-07-13 22:47:11 -0700

    Remove code duplication around version info.
    Text.Pandoc.App.CommandLineOptions and pandoc-cli/src/pandoc.hs
    had similar code for generating version information.
    
    To avoid duplication, we now export `versionInfo` from
    Text.Pandoc.App [API change]. (The function is reexported from the
    non-public module Text.Pandoc.App.CommandLineOptions.)  This function
    has three parameters that can be filled in when it is called by
    pandoc-cli.
    
    This change will make it simpler to revise version information.

--- src/Text/Pandoc/App/CommandLineOptions.hs
+++ src/Text/Pandoc/App/CommandLineOptions.hs
@@ -21,6 +21,7 @@ module Text.Pandoc.App.CommandLineOptions (
           , options
           , engines
           , setVariable
+          , versionInfo
           ) where
 import Control.Monad.Trans
 import Control.Monad.State.Strict
@@ -46,6 +47,7 @@ import System.IO (stdout)
 import Text.DocTemplates (Context (..), ToContext (toVal), Val (..))
 import Text.Pandoc
 import Text.Pandoc.Builder (setMeta)
+import Data.Version (showVersion)
 import Text.Pandoc.App.Opt (Opt (..), LineEnding (..), IpynbOutput (..),
                             DefaultsState (..), applyDefaults,
                             fullDefaultsPath, OptInfo(..))
@@ -193,13 +195,7 @@ handleOptInfo engine info = E.handle (handleError . Left) $ do
                      ,"text-styles"])
                   ,confNumFormat = Generic
                   ,confTrailingNewline = True} sty
-    VersionInfo -> do
-      defaultDatadir <- defaultUserDataDir
-      UTF8.hPutStrLn stdout
-       $ T.pack
-       $ "pandoc " ++ T.unpack pandocVersionText ++
-         "\nUser data directory: " ++ defaultDatadir ++
-         ('\n':copyrightMessage)
+    VersionInfo -> versionInfo [] Nothing ""
     Help -> do
       prg <- getProgName
       UTF8.hPutStr stdout (T.pack $ usageMessage prg options)
@@ -1269,3 +1265,21 @@ normalizePath fp =
 #else
 normalizePath = id
 #endif
+
+-- | Print version information with customizable features and scripting engine
+versionInfo :: [String] -> Maybe String -> String -> IO ()
+versionInfo features mbScriptingEngineName suffix = do
+  defaultDatadir <- defaultUserDataDir
+  let featuresLine = if null features
+                       then []
+                       else ["Features: " ++ unwords features]
+  let scriptingLine = case mbScriptingEngineName of
+                        Nothing -> []
+                        Just name -> ["Scripting engine: " ++ name]
+  UTF8.putStr $ T.unlines $ map T.pack $
+    ["pandoc " ++ showVersion pandocVersion ++ suffix] ++
+    featuresLine ++
+    scriptingLine ++
+    ["User data directory: " ++ defaultDatadir,
+     copyrightMessage]
+  exitSuccess
