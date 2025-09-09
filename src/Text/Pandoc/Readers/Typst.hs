056d9f79f509ec89da6090b93e6c02d27d518957
RayBB <RayBB@users.noreply.github.com>
2025-09-09 03:22:25 -0400

    add support for reading typst pagebreak

--- src/Text/Pandoc/Readers/Typst.hs
+++ src/Text/Pandoc/Readers/Typst.hs
@@ -396,6 +396,7 @@ blockHandlers = M.fromList
       getField "body" fields >>= pWithContents pBlocks)
   ,("pad", \_ _ fields ->  -- ignore paddingy
       getField "body" fields >>= pWithContents pBlocks)
+  ,("pagebreak", \_ _ _ -> pure $ B.divWith ("", ["pagebreak"], []) B.horizontalRule)
   ]
 
 inlineHandlers :: PandocMonad m =>
