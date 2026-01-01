6dcab340240fad7d4d5f02e642847b7c469665d1
John MacFarlane <jgm@berkeley.edu>
2025-12-31 19:30:40 -0500

    Split pandoc-wasm stuff into separate source, pandoc-wasm.hs.
    This is cleaner than relying on CPP.

---
+++ pandoc-cli/src/pandoc-wasm.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+{- |
+   Module      : Main
+   Copyright   : Copyright (C) 2006-2024 John MacFarlane
+   License     : GNU GPL, version 2 or above
+
+   Maintainer  : John MacFarlane <jgm@berkeley@edu>
+   Stability   : alpha
+   Portability : portable
+
+Parses command-line options and calls the appropriate readers and
+writers (wasm version).
+-}
+module Main where
+import qualified Control.Exception as E
+import Data.Maybe (fromMaybe)
+import Text.Pandoc.App ( convertWithOpts, Opt(..) )
+import PandocCLI.Lua
+import qualified Data.Text as T
+import Control.Exception
+import Foreign
+import Foreign.C
+import qualified Data.Aeson as Aeson
+import qualified Text.Pandoc.UTF8 as UTF8
+import qualified Data.Text.IO as TIO
+import qualified Data.ByteString.Lazy as BL
+import Text.Pandoc.Logging (showLogMessage)
+
+foreign export ccall "wasm_main" wasm_main :: Ptr CChar -> Int -> IO ()
+
+wasm_main :: Ptr CChar -> Int -> IO ()
+wasm_main raw_args_ptr raw_args_len =
+  E.catch act (\(err :: SomeException) -> writeFile "/stderr" (displayException err))
+  where
+    act = do
+      args <- peekCStringLen (raw_args_ptr, raw_args_len)
+      free raw_args_ptr
+      engine <- getEngine
+      let aesonRes = Aeson.eitherDecode (UTF8.fromStringLazy args)
+      case aesonRes of
+        Left e -> error e
+        Right opts -> do
+          let opts' = opts{ optInputFiles = Just $ fromMaybe ["/stdin"] (optInputFiles opts)
+                          , optOutputFile = Just $ fromMaybe "/stdout" (optOutputFile opts)
+                          , optLogFile = Just $ fromMaybe "/warnings" (optLogFile opts)
+                          }
+          convertWithOpts engine opts'
+          res <- Aeson.eitherDecode <$> BL.readFile "/warnings"
+          case res of
+            Left e -> writeFile "/stderr" e
+            Right msgs -> TIO.writeFile "/stderr" (T.unlines $ map showLogMessage msgs)
+
+-- This must be included or we get an error:
+main :: IO ()
+main = pure ()
--- pandoc-cli/src/pandoc.hs
+++ pandoc-cli/src/pandoc.hs
@@ -17,8 +17,7 @@ writers.
 module Main where
 import qualified Control.Exception as E
 import System.Environment (getArgs, getProgName)
-import Data.Maybe (fromMaybe)
-import Text.Pandoc.App ( convertWithOpts, defaultOpts, options, Opt(..)
+import Text.Pandoc.App ( convertWithOpts, defaultOpts, options
                        , parseOptionsFromArgs, handleOptInfo, versionInfo )
 import Text.Pandoc.Error (handleError)
 import Data.Monoid (Any(..))
@@ -26,23 +25,10 @@ import PandocCLI.Lua
 import PandocCLI.Server
 import Text.Pandoc.Scripting (ScriptingEngine(..))
 import qualified Data.Text as T
-
+import System.Exit
 #ifdef NIGHTLY
 import qualified Language.Haskell.TH as TH
 import Data.Time
-#endif
-
-#if defined(wasm32_HOST_ARCH)
-import Control.Exception
-import Foreign
-import Foreign.C
-import qualified Data.Aeson as Aeson
-import qualified Text.Pandoc.UTF8 as UTF8
-import qualified Data.Text.IO as TIO
-import qualified Data.ByteString.Lazy as BL
-import Text.Pandoc.Logging (showLogMessage)
-#else
-import System.Exit
 #endif
 
 #ifdef NIGHTLY
@@ -53,34 +39,6 @@ versionSuffix = "-nightly-" ++
 #else
 versionSuffix :: String
 versionSuffix = ""
-#endif
-
-#if defined(wasm32_HOST_ARCH)
-
-foreign export ccall "wasm_main" wasm_main :: Ptr CChar -> Int -> IO ()
-
-wasm_main :: Ptr CChar -> Int -> IO ()
-wasm_main raw_args_ptr raw_args_len =
-  E.catch act (\(err :: SomeException) -> writeFile "/stderr" (displayException err))
-  where
-    act = do
-      args <- peekCStringLen (raw_args_ptr, raw_args_len)
-      free raw_args_ptr
-      engine <- getEngine
-      let aesonRes = Aeson.eitherDecode (UTF8.fromStringLazy args)
-      case aesonRes of
-        Left e -> error e
-        Right opts -> do
-          let opts' = opts{ optInputFiles = Just $ fromMaybe ["/stdin"] (optInputFiles opts)
-                          , optOutputFile = Just $ fromMaybe "/stdout" (optOutputFile opts)
-                          , optLogFile = Just $ fromMaybe "/warnings" (optLogFile opts)
-                          }
-          convertWithOpts engine opts'
-          res <- Aeson.eitherDecode <$> BL.readFile "/warnings"
-          case res of
-            Left e -> writeFile "/stderr" e
-            Right msgs -> TIO.writeFile "/stderr" (T.unlines $ map showLogMessage msgs)
-
 #endif
 
 main :: IO ()
