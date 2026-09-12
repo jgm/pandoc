6c335dc4c93ea5ebf8f5d39759c661742fd6ab89
John MacFarlane <jgm@berkeley.edu>
2026-09-12 11:03:06 -0700

    diff-golden-tests.sh - print filename

--- tools/diff-golden-tests.sh
+++ tools/diff-golden-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-for f in `git status test/*/golden | grep modified: | sed -e 's/^.*: *//'`; do git cat-file -p HEAD:$f > $(basename $f) ; ./tools/diff-zip.sh $(basename $f) $f;  done
+for f in `git status test/*/golden | grep modified: | sed -e 's/^.*: *//'`; do echo "\n$f"; git cat-file -p HEAD:$f > $(basename $f) ; ./tools/diff-zip.sh $(basename $f) $f;  done
